a116ebdcbf8ce92adcc426dbaaf72a07988e65ab
Dave Matthews <matthews@greengenes.cit.cornell.edu>
2011-02-26 14:17:03 -0800

    1. Set mysql to "use strict" so loading enum columns with bad data    gives an error. 2. Many improvements in "Cluster Lines by Genotype", cluster_lines.php

--- R/temp/setupcluster.R
+++
@@ -1,4 +0,0 @@
-png("/srv/www/htdocs/matthews/tht/downloads/temp/linecluster.png", width=600, height=500)
-lineNames <- c("MOREX", "DOYCE", "PRICE", "THOROUGHBRED", "WYSOR", "NOMINI")
-nClust <- 5
-setwd("/srv/www/htdocs/matthews/tht/R")
