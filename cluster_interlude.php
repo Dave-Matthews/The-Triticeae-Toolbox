63bf6c00811addbb747e7d017ce70a6f798f3c93
Dave Matthews <matthews@greengenes.cit.cornell.edu>
2011-03-09 18:11:18 -0800

    Used Ajax instead of redirect (cluster_interlude.php) for the Please Wait.

--- cluster_interlude.php
+++ cluster_interlude.php
@@ -1,5 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
 <?php
 require 'config.php';
+//echo "<meta http-equiv='refresh' content='0; url=".$config['base_url']."cluster_lines1.php'>";
+header("refresh:0; url=".$config['base_url']."cluster_lines1.php"); 
+
 include($config['root_dir'].'includes/bootstrap.inc');
 include($config['root_dir'].'theme/admin_header.php');
 ?>
@@ -10,17 +15,16 @@ include($config['root_dir'].'theme/admin_header.php');
   <div class="section">
   <h3>Please wait ...</h3>
 
-  <!-- Strange that this is not working. See http://api.jquery.com/jQuery.get -->
+  <!-- Strange that this is not working. See http://api.jquery.com/jQuery.get 
   <script src="./jquery.js" type="text/javascript"></script>
   <script type="text/javascript">
   jQuery.get("cluster_lines.php");
-  </script>
+  </script> -->
 
 <?php
   $linecount = count($_SESSION['selected_lines']);
 echo "<br>Retrieving all marker alleles for <b>$linecount</b> lines.<br>";
 echo "Retrieval rate is ca. one minute for 500 lines (1.5 million alleles).";
-echo "<meta http-equiv='refresh' content='0; url=".$config['base_url']."cluster_lines.php'>";
 ?>
 
 </div></div></div>
