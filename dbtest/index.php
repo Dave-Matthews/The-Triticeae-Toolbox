0d3fe62788ad104549c7905fb7553ef44bbf816c
Dave Matthews <matthews@greengenes.cit.cornell.edu>
2011-03-28 13:02:17 -0700

    input_experiments_check_*.php: Fixed 'NULL' to NULL in UPDATE command. dbtest: Fixed the first two commands in the Database menu.

--- dbtest/index.php
+++ dbtest/index.php
@@ -1,49 +1,52 @@
 <?php
+require 'config.php';
+include($config['root_dir'].'includes/bootstrap.inc');
+include($config['root_dir'].'theme/normal_header.php');
+connect();
 
-	$linkID = mysql_connect("lab.bcb.iastate.edu", "yhames04", "gdcb07") or die(mysql_error());
-	mysql_select_db("sandbox_yhames04", $linkID) or die(mysql_error());
+/* We have connected */
 
-	/* We have connected */
+$tables = array();
+$i = 0;
 
-	$tables = array();
-	$i = 0;
+$query = mysql_query("SHOW TABLES");
+while($row = mysql_fetch_row($query)) {
 
-	$query = mysql_query("SHOW TABLES");
-	while($row = mysql_fetch_row($query)) {
+  $tables[$i] = $row[0];
+  $i++;
 
-		$tables[$i] = $row[0];
-		$i++;
+ }
 
-	}
+for($i=0; $i<count($tables); $i++) {
 
-	for($i=0; $i<count($tables); $i++) {
+  $query = mysql_query("DESCRIBE $tables[$i]");
+  $fc = 1;
 
-		$query = mysql_query("DESCRIBE $tables[$i]");
-		$fc = 1;
 
-		echo "<strong>$tables[$i]</strong>: <a href=\"viewtable.php?table=$tables[$i]\">View Contents</a> <br />";// ---- <a href=\"deleteTable.php?table=$tables[$i]\">Delete Contents</a> <br />";
-		echo "<table border=\"1\">\n<tr>\n\t";
+    echo "<strong>$tables[$i]</strong>: <a href='".$config['base_url']."dbtest/viewtable.php?table=$tables[$i]'>View Contents</a> <br />";
+    // ---- <a href=\"deleteTable.php?table=$tables[$i]\">Delete Contents</a> <br />";
+    echo "<table border=\"1\">\n<tr>\n\t";
 
-		while($row = mysql_fetch_assoc($query)) {
+    while($row = mysql_fetch_assoc($query)) {
+      
+    if($fc == 1) {
 
-			if($fc == 1) {
+      foreach($row as $k=>$v) {
+	echo "\n\t\t<td><strong>$k</strong></td>";
+      }
+    }
 
-				foreach($row as $k=>$v) {
-					echo "\n\t\t<td><strong>$k</strong></td>";
-				}
-			}
+    echo "\n</tr>\n<tr>\n\t";
 
-			echo "\n</tr>\n<tr>\n\t";
-
-			foreach($row as $k=>$v) {
+    foreach($row as $k=>$v) {
 				
-				echo "\n\t\t<td>$v</td>";
+      echo "\n\t\t<td>$v</td>";
 
-			}
-			$fc++;
-		}
+    }
+    $fc++;
+  }
 
-		echo "\n</tr>\n</table>\n<br />";
-	}
+  echo "\n</tr>\n</table>\n<br />";
+ }
 
 ?>
