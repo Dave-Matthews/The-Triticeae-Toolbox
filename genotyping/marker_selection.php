886c95eda145adfce96fbcbf475a094890264a58
Clay Birkett <clb343@cornell.edu>
2012-05-22 11:35:47 -0700

    modify marker selection page so that it does not use gbrowse if over 500 markers selected

--- genotyping/marker_selection.php
+++ genotyping/marker_selection.php
@@ -2,6 +2,8 @@
 /*
 * Logged in page initialization
 *
+* 16mar12 dem Allow selecting markers that are not in maps.
+*             Un-require all marker names to also be in marker_synonyms.value.
 * 9/2/2010   J.Lee modify to add new snippet Gbrowse tracks
 * 8/29/2010  J.Lee modify to not use iframe for link to Gbrowse   
 */
@@ -37,7 +39,9 @@ if ( isset($_POST['selMarkerstring']) && $_POST['selMarkerstring'] != "" ) {
   // Get the marker uids.
   $selmkrs = array();
   foreach ($selmkrnames as $mkrnm) {
-    $sql = "select marker_uid from marker_synonyms where value = '$mkrnm'";
+    //$sql = "select distinct marker_uid from marker_synonyms where value = '$mkrnm'";
+    $sql = "select distinct marker_uid from marker_synonyms where value = '$mkrnm' UNION
+            select marker_uid from markers where marker_name = '$mkrnm'";
     $r = mysql_query($sql);
     if (mysql_num_rows($r) == 0)
       echo "<font color=red>\"$mkrnm\" not found.</font><br>";
@@ -60,7 +64,8 @@ if ( isset($_POST['selMarkerstring']) && $_POST['selMarkerstring'] != "" ) {
     if (!isset($mapids) || !is_array($mapids))
       $mapids = array();
   foreach ($selmkrs as $mkr) {
-    $sql = "select map_uid from markers_in_maps where marker_uid = $mkr";
+    $sql = "select distinct map_uid from markers_in_maps where marker_uid = $mkr";
+    //$sql = "select distinct map_uid from markers where marker_uid = $mkr";
     $r = mysql_query($sql);
     $row = mysql_fetch_row($r);
     if (! in_array($row[0], $mapids))
@@ -75,8 +80,10 @@ if (isset($_POST['selMkrs']) || isset($_POST['selbyname'])) {
       $selmkrs=$_POST['selMkrs'];
     else {
       $selbyname = $_POST['selbyname'];
-      $sql = "select m.marker_uid from markers as m inner join
-markers_in_maps as mm using(marker_uid) where mm.map_uid=$mapid and
+/*       $sql = "select m.marker_uid from markers as m inner join */
+/* markers_in_maps as mm using(marker_uid) where mm.map_uid=$mapid and */
+/* m.marker_name='" . mysql_real_escape_string($selbyname) . "'"; */
+      $sql = "select m.marker_uid from markers where
 m.marker_name='" . mysql_real_escape_string($selbyname) . "'";
       $sqlr = mysql_fetch_assoc(mysql_query($sql));
       $selmkrs = array($sqlr['marker_uid']);
@@ -120,19 +127,27 @@ if (isset($_SESSION['clicked_buttons']) && count($_SESSION['clicked_buttons']) >
   reset($mapids);
 
   $chrlist = array();
+  $markerlist = array();
+  $count_markers = 0;
   foreach ($_SESSION['clicked_buttons'] as $mkruid) {
+    $count_markers++;
     $mapid = current($mapids);
     next($mapids);
-    $sql = "select m.marker_name, mm.chromosome
-from markers as m inner join markers_in_maps as mm using(marker_uid)
-where marker_uid=$mkruid" . ($mapid ? " and mm.map_uid=$mapid":"");
+/*     $sql = "select m.marker_name, mm.chromosome */
+/* from markers as m inner join markers_in_maps as mm using(marker_uid) */
+/* where marker_uid=$mkruid" . ($mapid ? " and mm.map_uid=$mapid":""); */
+    $sql = "select marker_name from markers where marker_uid=$mkruid";
     $result=mysql_query($sql)
       //        or die("invalid marker uid\n");
       or die(mysql_error());
     while ($row=mysql_fetch_assoc($result)) {
       $selval=$row['marker_name'];
-      array_push($chrlist, $row['chromosome']);
-      print "<option value='$mkruid'>$selval</option>\n";
+      $selchr=$row['chromosome'];
+      if(! in_array($selval,$markerlist)) {
+        array_push($markerlist, $selval);
+        array_push($chrlist, $selchr);
+        print "<option value='$mkruid'>$selval</option>\n";
+      }
     }
   }
   $chrlist = array_unique($chrlist);
@@ -144,6 +159,9 @@ var mlist = \$j('#mlist option').map(function () { return \$j(this).text(); });
 </script>";
 
  // Show GBrowse maps.
+ if ($count_markers > 500) {
+    echo "too many markers to display in Gbrowse<br>\n";
+ } else {
  foreach ($chrlist as $chr) {
    echo "<div id='gbrowse_$chr'></div>\n";
    echo <<<EOD
@@ -194,6 +212,7 @@ var mlist = \$j('#mlist option').map(function () { return \$j(this).text(); });
 </script>
 EOD;
  }
+ }
  print "</td></tr></table>\n";
  print "<p><input type='submit' value='Remove marker' style='color: blue' /></p>";
  print "</form>";
@@ -231,7 +250,7 @@ EOD;
   </thead>
   <tbody>
   <tr><td>
-  <select name='mapname' size=10 onfocus="DispMapSel(this.value)" onchange="DispMapSel(this.value)">
+  <select name='mapname' size=10 onClick="DispMapSel(this.value)" onchange="DispMapSel(this.value)">
 <?php
 $result=mysql_query("select map_name from map") or die(mysql_error);
 while ($row=mysql_fetch_assoc($result)) {
