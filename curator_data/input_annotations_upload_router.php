182daf2c917608e25e68bb760bf755d86671c853
Clay Birkett <clb343@cornell.edu>
2012-09-28 13:21:16 -0700

    add page and link to edit phenotype_experiment_info table

--- curator_data/input_annotations_upload_router.php
+++ curator_data/input_annotations_upload_router.php
@@ -36,7 +36,7 @@ class Annotations {
     global $config;
     include($config['root_dir'] . 'theme/admin_header.php');
 
-    echo "<h2>Add Phenotype Experiment Annotations</h2>"; 
+    echo "<h2>Add Phenotype Trial Annotations</h2>"; 
     $this->type_Annotation_Name();
     $footer_div = 1;
     include($config['root_dir'].'theme/footer.php');
@@ -50,11 +50,14 @@ class Annotations {
   a {text-decoration: none;}
 </style>
 
-<ul>
-<a href="<?php echo $config['base_url']; ?>curator_data/input_annotations_upload_excel.php"><li><b>Upload an Excel (.xls) File</b></li></a><br>
-<a href="<?php echo $config['base_url']; ?>curator_data/input_annotations_upload_text.php"><li><b>Upload a Tab Delimited(.txt) File</b></li></a><br>
+<table>
+<tr><td><a href="<?php echo $config['base_url']; ?>curator_data/input_annotations_upload_excel.php"><b>Upload an Excel (.xls) File</b></a>
+<tr><td><a href="<?php echo $config['base_url']; ?>login/edit_trials.php"><b>Edit Trial Annotations</b></a><td>Genotype and Phenotype experiments
+<tr><td><a href="<?php echo $config['base_url']; ?>login/edit_pheno_expr.php"><b>Edit Phenotype Experiment</b></a><td>Phenotype specific annotation
+</table>
+
+<!-- <a href="<?php echo $config['base_url']; ?>curator_data/input_annotations_upload_text.php"><li><b>Upload a Tab Delimited(.txt) File</b></li></a><br> -->
 <!-- <a href="<?php echo $config['base_url']; ?>curator_data/delete_experiment.php"><li><b>Delete an experiment</b></li></a> -->
-</ul>	
 		
 <?php
     } /* end of type_Annotation_Name function*/
