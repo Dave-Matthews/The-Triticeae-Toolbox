9b5af6824743715d91de6aed016b84982100d273
Clay Birkett <clb343@cornell.edu>
2013-02-24 19:40:10 -0800

    change smoothing function in CSR, add description of formula, indicate CSR in phenotype experiments

--- curator_data/csr.js
+++ curator_data/csr.js
@@ -50,36 +50,44 @@ function update_w2() {
 }
 
 function update_f1() {
+  var desc = "";
   var e = document.getElementById("formula1");
   formula1 = e.options[e.selectedIndex].value;
   if (formula1 == "NWI1") {
     w2 = 970;
     w1 = 900;
     formula2 = "(W2-W1)/(W1+W2)";
+    desc = "Normalized Water Index";
   } else if (formula1== "NWI3") {
     w2 = 970;
     w1 = 880;
     formula2 = "(W2-W1)/(W1+W2)";
+    desc = "Normalized Water Index";
   } else if (formula1 == "NDVI") {
     w2 = 900;
     w1 = 680;
     formula2 = "(W2-W1)/(W1+W2)";
+    desc = "<a target='_blank' href=http://en.wikipedia.org/wiki/Normalized_Difference_Vegetation_Index>Normalized Difference Vegetation Index</a>";
   } else if (formula1 == "NDVIR") {
     w2 = 780;
     w1 = 670;
     formula2 = "(W2-W1)/(W1+W2)";
+    desc = "<a target='_blank' href=http://en.wikipedia.org/wiki/Normalized_Difference_Vegetation_Index>Red Normalized Difference Vegetation Index</a>";
   } else if (formula1 == "NDVIG") {
     w2 = 780;
     w1 = 550;
     formula2 = "(W2-W1)/(W1+W2)";
+     desc = "<a target='_blank' href=http://en.wikipedia.org/wiki/Normalized_Difference_Vegetation_Index>Green Normalized Difference Vegetation Index</a>";
   } else if (formula1 == "SR") {
     w2 = 900;
     w1 = 680;
     formula2 = "(W2-W1)";
+    desc = "Simple Ratio";
   }
   document.getElementById("W1").value = w1;
   document.getElementById("W2").value = w2;
   document.getElementById("formula2").value = formula2;
+  document.getElementById("formdesc").innerHTML = desc;
   formula2 = encodeURIComponent(formula2); 
 }
 
@@ -91,12 +99,12 @@ function update_f2() {
 function update_smooth() {
   var e = document.getElementById("smooth");
   smooth = e.options[e.selectedIndex].value;
-  if (smooth == 0) {
+  if (smooth == "0") {
     document.getElementById("smooth2").innerHTML = "no smoothing";
-  } else if (smooth == 5) {
-    document.getElementById("smooth2").innerHTML = "average at wavelength and 5 points on either side";
-  } else if (smooth == 10) {
-    document.getElementById("smooth2").innerHTML = "average at wavelength and 10 points on either side";
+  } else if (smooth == "5") {
+    document.getElementById("smooth2").innerHTML = "median value using a window of 11 samples";
+  } else if (smooth == "10") {
+    document.getElementById("smooth2").innerHTML = "median value using a window of 21 samples";
   }
 }
 
--- view_search_yr2.php
+++ view_search_yr2.php
@@ -5,60 +5,67 @@ include($config['root_dir'].'includes/bootstrap.inc');
 include($config['root_dir'].'theme/normal_header.php');
 connect();
 
+echo "<h2>Available Datasets</h2><p>";
 $year=$_GET['year'];
 $sql_year = mysql_real_escape_string($year);
 
+$sql="SELECT experiments.experiment_uid from csr_measurement, experiments
+      WHERE experiments.experiment_uid = csr_measurement.experiment_uid
+      AND experiment_year='$sql_year'";
+$result_trialcode=mysql_query($sql) or die(mysql_error());
+while($row_trialcode=mysql_fetch_array($result_trialcode)) {
+  $uid = $row_trialcode['experiment_uid'];
+  $csr_list[$uid] = 1;
+}
 
-$sql="SELECT e.trial_code, e.experiment_year, e.traits, et.experiment_type_name
-					FROM experiments as e, experiment_types as et
-					WHERE e.experiment_year='$sql_year'
-					AND et.experiment_type_uid = e.experiment_type_uid";
-
+$sql="SELECT e.trial_code, e.experiment_year, e.traits, et.experiment_type_name, e.experiment_uid
+		FROM experiments as e, experiment_types as et
+		WHERE e.experiment_year='$sql_year'
+		AND et.experiment_type_uid = e.experiment_type_uid";
 if (!authenticate(array(USER_TYPE_PARTICIPANT,
 			USER_TYPE_CURATOR,
 			USER_TYPE_ADMINISTRATOR)))
   $sql .= " AND data_public_flag > 0";
 $sql .= " order by e.experiment_year,e.trial_code ASC";
 $result_trialcode=mysql_query($sql) or die(mysql_error());
+$num_rows = mysql_num_rows($result_trialcode);
+if ($num_rows ==0){
 ?>
-<h3>Available Datasets</h3>
-<p>
-	<table cellpadding="0" cellspacing="0">
-	<tr>
-		<th>Year</th>
-		<th>Trial Name</th>
-		<th>Traits</th>
-		
-	</tr>
-	<?php
-	$num_rows = mysql_num_rows($result_trialcode);
-	if ($num_rows ==0){
-		?>	<div class="section">
-			<p> There are no publicly available datasets for this year in THT at this time. Participants in the BarleyCAP project need to login to see additional datasets.</p>
-		</div>
-		<?php
-	} else {
-		while($row_trialcode=mysql_fetch_array($result_trialcode))
-		{
-			$trial_code=$row_trialcode['trial_code'];
-			$year=$row_trialcode['experiment_year'];
-			$traits=$row_trialcode['traits'];
-			$filename="data/".$trial_code.".xls";
-			$experiment_type = $row_trialcode['experiment_type_name'];
-			if ($experiment_type=='phenotype') {
-			  echo( "<tr> <td>$year</td> <td><a href='display_phenotype.php?trial_code=$trial_code'>$trial_code</a> </td> <td>$traits</td> </tr>");
-			} elseif ($experiment_type=='genotype') {
-			  echo( "<tr> <td>$year</td> <td><a href='display_genotype.php?trial_code=$trial_code'>$trial_code</a> </td> <td>$traits</td> </tr>");
-			}
-		}
-	}?>
-	
-	
-	</tbody>
-	</table>
-	</p>
-
+<div class="section">
+  <p> There are no publicly available datasets for this year in T3 at this time. 
+    Registered users may see additional datasets after signing in.
+</div>
 <?php
+} 
+else {
+?>
+<table cellpadding="0" cellspacing="0">
+  <tr>
+    <th>Year</th>
+    <th>Trial Name</th>
+    <th>Traits</th>
+  </tr>
+<?php
+
+    while($row_trialcode=mysql_fetch_array($result_trialcode))
+      {
+        $uid=$row_trialcode['experiment_uid'];
+	$trial_code=$row_trialcode['trial_code'];
+	$year=$row_trialcode['experiment_year'];
+	$traits=$row_trialcode['traits'];
+	$filename="data/".$trial_code.".xls";
+	$experiment_type = $row_trialcode['experiment_type_name'];
+        if (isset($csr_list[$uid])) {
+          $traits = "CSR Data<br>$traits";
+        }
+	if ($experiment_type=='phenotype') {
+	  echo( "<tr> <td>$year</td> <td><a href='display_phenotype.php?trial_code=$trial_code'>$trial_code</a> </td> <td>$traits</td> </tr>");
+	} elseif ($experiment_type=='genotype') {
+	  echo( "<tr> <td>$year</td> <td><a href='display_genotype.php?trial_code=$trial_code'>$trial_code</a> </td> <td>$traits</td> </tr>");
+	}
+      }
+}
+echo "</table>";
 $footer_div = 1;
 include($config['root_dir'].'theme/footer.php'); ?>
 </body>
