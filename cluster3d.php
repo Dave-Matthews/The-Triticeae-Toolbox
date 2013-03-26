080b86fc69a36a765d5a8caa26a635b398d85e5b
Clay Birkett <clb343@cornell.edu>
2013-03-26 07:45:32 -0700

    add marker and line filtering to cluster 3D page

---
+++ cluster3d.php
@@ -0,0 +1,218 @@
+<?php
+require 'config.php';
+include($config['root_dir'].'includes/bootstrap.inc');
+include($config['root_dir'].'theme/admin_header.php');
+connect();
+?>
+
+<div id="primaryContentContainer">
+  <img id='spinner' src='./images/progress.gif' alt='Working...' style='display:none;'>
+  <div id="primaryContent">
+  <h1>Cluster Lines by Genotype, SVD</h1>
+  <div class="section">
+
+<?php
+$nclusters = $_GET['clusters'];
+
+// Timestamp for names of temporary files.
+$time = $_GET['time'];
+$min_maf = $_GET['mmaf'];
+$max_missing = $_GET['mmm'];
+$max_miss_line = $_GET['mml'];
+$querytime = $_SESSION['timmer'];
+
+// Store the input parameters in file setupclust3d.txt.
+if (! file_exists('/tmp/tht')) mkdir('/tmp/tht');
+$setup = fopen("/tmp/tht/setupclust3d.txt".$time, "w");
+fwrite($setup, "lineNames <-c('')\n");
+fwrite($setup, "nClust <- $nclusters\n");
+fwrite($setup, "setwd(\"/tmp/tht/\")\n");
+fwrite($setup, "mrkDataFile <-c('mrkData.csv".$time."')\n");
+fwrite($setup, "clustInfoFile<-c('clustInfo.txt".$time."')\n");
+fwrite($setup, "clustertableFile <-c('clustertable.txt".$time."')\n");
+fwrite($setup, "clust3dCoords<-c('clust3dCoords.csv".$time."')\n");
+fclose($setup);
+
+$starttime = time();
+//   For debugging, use this to show the R output:
+//   (Regardless, R error messages will be in the Apache error.log.)
+//echo "<pre>"; system("cat /tmp/tht/setupclust3d.txt$time R/Clust3D.R | R --vanilla 2>&1");
+exec("cat /tmp/tht/setupclust3d.txt$time R/Clust3D.R | R --vanilla > /dev/null 2> /tmp/tht/cluster3d.txt$time");
+$elapsed = time() - $starttime;
+
+/*
+ * Show the graphic.
+ */
+if (!file_exists("/tmp/tht/clust3dCoords.csv".$time)) {
+  echo "Error - R script failed<br>\n";
+  $h = fopen("/tmp/tht/cluster3d.txt".$time,"r");
+  while ($line=fgets($h)) {
+    echo "$line<br>\n";
+  }
+  fclose($h);
+  die();
+}
+?>
+    <script type="text/javascript" src="X3DOM/x3dom-full.js"></script>
+    <link rel="stylesheet" type="text/css" href="X3DOM/x3dom.css" />
+    <!-- Box for line names to appear in -->
+    <style type="text/css">
+      #myoutput {
+      position: absolute;                                                          
+      float: left;                                                                 
+      z-index: 1;                                                                  
+      top: 5px;                                                                    
+      left: 5px;                                                                   
+      width: 10em;                                                                 
+      height: 2em;                                                                 
+      border: none;
+      background-color: white;                                                   
+      text-align: left;
+      font-size: 18px;
+      }
+    </style>
+
+    <x3d xmlns="http://www.x3dom.org/x3dom" showStat="false" showLog="true" showProgress="true" x="0px" y="0px" width="500px" height="500px">
+
+      <div id="myoutput"></div>
+      <scene>
+	<viewpoint position='0 0 10' orientation="0 40 40 0" fieldOfView="0.785398"></viewpoint>
+<?php
+// Define the colors for the plotting symbols.
+$color = array('','black','red','limegreen','blue','cyan','magenta','orange','#ffff00');
+for ($i=1; $i <= count($color); $i++) {
+  echo "<appearance DEF='_$i'>";
+  echo "<material diffuseColor='$color[$i]' specularColor='.2 .2 .2' transparency='0.3'></material>";
+  echo "</appearance>";
+}
+
+$coords = file("/tmp/tht/clust3dCoords.csv".$time);
+$coords = preg_replace("/\n/", "", $coords);
+
+// Get the ranges of the PCA values.
+for ($i=0; $i<count($coords); $i++) {
+  $coords[$i] = explode("\t", $coords[$i]);
+  $xvals[] = $coords[$i][2];
+  $yvals[] = $coords[$i][3];
+  $zvals[] = $coords[$i][4];
+}
+$xrange = max($xvals) - min($xvals);
+$yrange = max($yvals) - min($yvals);
+$zrange = max($zvals) - min($zvals);
+
+for ($i=0; $i<count($coords); $i++) {
+  $name = str_replace("\"", "", $coords[$i][0]);
+  $clusternumber = $coords[$i][1];
+  $x = 5 * $coords[$i][2] / $xrange;
+  $y = 5 * $coords[$i][3] / $yrange;
+  $z = 5 * $coords[$i][4] / $zrange;
+  echo "
+     <transform translation='$x $y $z'>
+       <shape DEF='$name'>
+	 <appearance USE='_$clusternumber'>
+	 </appearance>
+<!--	 <text string='$name'><fontstyle family='Helvetica' size='12'></fontstyle></text> -->
+	 <sphere radius='0.1' 	
+  	         onmouseover=\"document.getElementById('myoutput').innerHTML = '$name'\" 
+	         onclick=\"alert('Line name: $name')\">
+	 </sphere>
+       </shape>
+     </transform>
+     ";
+  }
+?>
+      </scene>
+    </x3d>
+</div>
+
+  <div style="position: absolute; left: 765px; top: 520px; width: 180px;">
+    <b>r</b>: Reset.<br>
+    <b>Doubleclick</b>: Re-center rotation.<br>
+    <a href="http://x3dom.org/docs/dev/navigation.html" target="_blank">Other commands...</a>
+    <br><br><p style="font-size: 8pt">
+      <b>Browsers:</b><br>
+      <b>Firefox</b> and <b>Chrome</b> work well.<br>
+      <b>Internet Explorer</b> requires <a href="http://get.adobe.com/flashplayer/">Flash Player 11</a> (new version).<br>
+      <b>Mac Safari</b>: Set "Enable WebGL" in the 
+      <span onclick = "alert('To get Safari to show the Develop menu, go to Preferences.../Advanced. \n\'Show Develop menu\' is at the bottom of the dialog box.')" style = "text-decoration: underline">
+	Develop menu.</span><br>
+    <p style="font-size: 8pt">
+	Graphics from <a href="http://www.x3dom.org">www.x3dom.org</a>
+  </div>
+
+<div style="clear:both">
+<!-- For testing only: Show elapsed times. -->
+Query time = <?php echo $querytime ?> s<br>
+Analysis time = <?php echo $elapsed ?> s<br>
+<style type="text/css">
+  table th {text-align: center;}
+  table td {text-align: center;}
+</style>
+
+<script type="text/javascript" src="cluster.js"></script>
+<?php
+/* Show table of cluster members.  */
+$clustInfo = file("/tmp/tht/clustInfo.txt".$time);
+$clustInfo = preg_replace("/\n/", "", $clustInfo);
+sort($clustInfo);
+
+for ($i=0; $i<count($clustInfo); $i++) {
+  $clustInfo[$i] = explode(", ", $clustInfo[$i]);
+  $clustsize[$clustInfo[$i][0]] = $clustInfo[$i][2];
+  $clustlist[$clustInfo[$i][0]] .= $clustInfo[$i][1].", ";
+ }
+$clustertable = file("/tmp/tht/clustertable.txt".$time);
+$clustertable = preg_replace("/\n/", "", $clustertable);
+// Remove the first row, "x".
+array_shift($clustertable);
+for ($i=0; $i<count($clustertable); $i++) {
+  $row = explode("\t", $clustertable[$i]);
+  $contents[$row[1]] .= $row[0].", ";
+}
+// Modify yellow a bit to show up better in text.
+$color = array('black','red','green','blue','cyan','magenta','orange','#cccc00');
+
+print "<form action='cluster_lines3d.php' method='POST'>";
+print "<table width=700 style='background-image: none; font-weight: bold;'>";
+print "<thead><tr><th>&nbsp;</th><th>Cluster</th><th>Count</th><th>Lines</th></tr></thead>";
+for ($i=1; $i<count($clustsize)+1; $i++) {
+  $total = $total + $clustsize[$i];
+  print "<tr style='color:".$color[$i-1]."';'>";
+  print "<td><input type='checkbox' name='mycluster[]' value=$i></td>";
+  print "<td>$i</td>";
+  print "<td>$clustsize[$i]</td>";
+  print "<td style='text-align: left'>".trim($contents[$i],', ')."</td>";
+  print "</tr>";
+ }
+print "<tr><td></td><td>Total:</td><td>$total</td></tr>";
+?>
+</table>
+    <p>
+    How many clusters? <input type=text id='clusters' name="clusters" value=<?php echo $nclusters ?> size="1">
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    Minimum MAF &ge; <input type="text" name="mmaf" id="mmaf" size="2" value="<?php echo ($min_maf) ?>" />%
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        Remove markers missing &gt; <input type="text" name="mmm" id="mmm" size="2" value="<?php echo ($max_missing) ?>" />% of data
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        Remove lines missing &gt; <input type="text" name="mml" id="mml" size="2" value="<?php echo ($max_miss_line) ?>" />% of data
+<br>
+<?php
+echo "<table>";
+$count = count($_SESSION['filtered_markers']);
+echo "<tr><td>markers<td>$count\n";
+$count = count($_SESSION['filtered_lines']);
+echo "<tr><td>lines<td>$count\n";
+echo "</table>";
+print "<p>Select the clusters you want to use. ";
+print "<input type = 'hidden' name = 'time' value = $time>";
+print "<input type=button value='Re-cluster' onclick='javascript:recluster($time)'>";
+print "</form>";
+
+// Clean up old files, older than 1 day.
+system("find /tmp/tht -mtime +1 -name 'clustertable.txt*' -delete");
+system("find /tmp/tht -mtime +1 -name 'mrkData.csv*' -delete");
+
+print "</div></div></div>";
+$footer_div=1;
+include($config['root_dir'].'theme/footer.php');
+?>
--- cluster_getalleles.php
+++ cluster_getalleles.php
@@ -1,137 +1,177 @@
 <?php
 require 'config.php';
-include($config['root_dir'].'includes/bootstrap.inc');
+//Need write access to update the cache table.
+//include($config['root_dir'].'includes/bootstrap.inc');
+include($config['root_dir'].'includes/bootstrap_curator.inc');
+include($config['root_dir'].'downloads/marker_filter.php');
+
 connect();
-?>
 
-<?php
-
-print "<div class='boxContent'>";
-$selectedcount = count($_SESSION['selected_lines']);
-echo "<h3><font color=blue>Currently selected lines</font>: $selectedcount</h3>";
-if ($selectedcount != 0) {
-  print "<textarea rows = 9>";
   foreach ($_SESSION['selected_lines'] as $lineuid) {
     $result=mysql_query("select line_record_name from line_records where line_record_uid=$lineuid") or die("invalid line uid\n");
     while ($row=mysql_fetch_assoc($result)) {
       $selval=$row['line_record_name'];
-      print "$selval\n";
     }
   }
-  print "</textarea>";
- }
-// Clean up all old copies.
-// No, bad idea, it could be another user's file.  Use a cron job.
-//array_map("unlink", glob($config['root_dir']."downloads/temp/clustertable.txt*"));
 
-// Adapted from download/downloads.php:
-// 2D array of alleles for all markers x currently selected lines
+$starttime = time();
+$selected_lines = $_SESSION['selected_lines'];
+$min_maf = $_GET['mmaf'];
+$max_missing = $_GET['mmm'];
+$max_miss_line = $_GET['mml'];
+calculate_af($selected_lines, $min_maf, $max_missing, $max_miss_line);
+
 if (!isset ($_SESSION['selected_lines']) || (count($_SESSION['selected_lines']) == 0) ) {
   // No lines selected so prompt to get some.
   echo "<a href=".$config['base_url']."pedigree/line_selection.php>Select lines.</a> ";
   echo "(Patience required for more than a few hundred lines.)";
- }
- else {
-   $lines = implode(",", $_SESSION['selected_lines']);
-   $delimiter =",";
-      
-   // Get all markers that have allele data, and their global allele frequencies.
-   $sql = "select markers.marker_uid, marker_name, avg(aa_freq), avg(ab_freq)
-    from markers, allele_frequencies
-    where markers.marker_uid = allele_frequencies.marker_uid
-    group by marker_name
-    order by markers.marker_uid";
-   $res = mysql_query($sql) or die(mysql_error());
-   while ($row = mysql_fetch_array($res)){
-     $marker_names[] = $row["marker_name"];
-     $marker_uid[] = $row["marker_uid"];
-     // Calculate the mean frequency of A over all this marker's alleles in the database.
-     $afreq = number_format($row["avg(aa_freq)"] + 0.5*($row["avg(ab_freq)"]), 3);
-     // "Empty" array of default allele scores, to be overwritten with the 
-     // germplasm line's allele if it was determined.
-     $empty[$row["marker_name"]] = $afreq;
-     $outputheader .= $row["marker_name"].$delimiter;
-   }
-   // Save the list of marker names to the output file.
-   $outputheader = trim($outputheader, ",")."\n";
-   // Make the filename unique to deal with concurrency.
-   $time = $_GET['time'];
-   /* $outfile = $config['root_dir']."downloads/temp/mrkData.csv".$time; */
-   if (! file_exists('/tmp/tht')) mkdir('/tmp/tht');
-   $outfile = "/tmp/tht/mrkData.csv".$time;
-   file_put_contents($outfile, $outputheader);
+} elseif (!isset ($_SESSION['filtered_lines'])) {
+  echo "Error: filtering routine did not work<br>\n";
+  die();
+} else {
+  $sel_lines = implode(",", $_SESSION['filtered_lines']);
+  $delimiter =",";
+  // Adapted from download/downloads.php:
+  // 2D array of alleles for all markers x currently selected lines
 
-   $markers = implode(",",$marker_uid);
-   $lookup = array(
-		  'AA' => '1',
-		  'BB' => '0',
-		  'AB' => '0.5',
-		  '--' => '--'
-		  );
-   // Get the alleles for currently selected lines, all genotyped markers.	
-   $sql = "SELECT lr.line_record_name, m.marker_name AS markername,
-                    CONCAT(a.allele_1,a.allele_2) AS value
-  	  FROM
-            markers as m,
-            line_records as lr,
-            alleles as a,
-            tht_base as tb,
-            genotyping_data as gd
-	  WHERE
-            a.genotyping_data_uid = gd.genotyping_data_uid
-	      AND m.marker_uid = gd.marker_uid
-	      AND gd.marker_uid IN ($markers)
-	      AND tb.line_record_uid = lr.line_record_uid
-	      AND gd.tht_base_uid = tb.tht_base_uid
-              AND lr.line_record_uid IN ($lines)
-	  ORDER BY lr.line_record_name, m.marker_uid";
-   $starttime = time();
-   $res = mysql_query($sql) or die(mysql_error());
-   $elapsed = time() - $starttime;
-   $numrows = number_format(mysql_num_rows($res));
-   echo "<p>Query time: $elapsed sec<br>";
-   echo "$numrows alleles<br>";
+  // Get all markers that have allele data, in marker_uid order as they are in allele_byline.alleles.
+  $sql = "select marker_uid, marker_name from allele_byline_idx order by marker_uid";
+  $res = mysql_query($sql) or die(mysql_error());
+  while ($row = mysql_fetch_row($res)) {
+    $markerids[] = $row[0];
+    // First row of output file mrkData.csv is list of marker names.
+    $outputheader .= $row[1] . $delimiter;
+  }
 
-  $outarray = $empty;
-  $cnt = 0;
-  while ($row = mysql_fetch_array($res)){
-    // First time through loop.
-    if ($cnt==0) {
-      $cnt = 1;
-      $last_line = $row['line_record_name'];
+  // Create cache table if necessary.
+  $n = mysql_num_rows(mysql_query("show tables like 'allele_byline_clust'"));
+  if ($n == 0) {
+    $sql = "create table allele_byline_clust (
+	      line_record_uid int(11) NOT NULL,
+              line_record_name varchar(50),
+	      alleles TEXT  COMMENT 'Up to 2^16 (65K) characters. Use MEDIUMTEXT for 2^24.',
+              updated_on timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	      PRIMARY KEY (line_record_uid)
+	    ) COMMENT 'Cache created from table allele_byline.'";
+    $res = mysql_query($sql) or die (mysql_error());
+    $update = TRUE;
+  }
+  else {
+    // Update cache table if necessary. Empty?
+    if(mysql_num_rows(mysql_query("select * from allele_byline_clust")) == 0)
+      $update = TRUE;
+    // Out of date?
+    $sql = "select if( datediff(
+	    (select max(updated_on) from allele_frequencies),
+	    (select max(updated_on) from allele_byline_clust)
+  	  ) > 0, 'need_update', 'okay')";
+    $need = mysql_grab($sql);
+    if ($need == 'need_update') $update = TRUE;
+  }
+  if ($update) {
+    echo "Updating table allele_byline_clust...<p>";
+    set_time_limit(3000);  // Default 30sec runs out in ca. line 105. So does 300.
+    mysql_query("truncate table allele_byline_clust") or die(mysql_error());
+    $lookup = array('AA' => '1',
+		    'BB' => '0',
+		    'AB' => '0.5');
+    // Compute global allele frequencies.
+    $sql = "select marker_uid, aa_cnt, ab_cnt, total from allele_frequencies";
+    $res = mysql_query($sql) or die(mysql_error());
+    while ($row = mysql_fetch_array($res)){
+      $aa_sum[$row[0]] += $row[1];
+      $ab_sum[$row[0]] += $row[2];
+      $total_sum[$row[0]] += $row[3];
     }
-    if ($last_line != $row['line_record_name']) {  
-      // Done collecting alleles for this germplasm line.  Output it.
-      $mname = $row['markername'];				
-      if ($lookup[$row['value']] != "--") {
-	// (For missing data, use the default value.)
-	$outarray[$mname] = $lookup[$row['value']];
-      }
-      $outarray = implode($delimiter,$outarray);
-      file_put_contents($outfile, $last_line.$delimiter.$outarray."\n", FILE_APPEND);
-      // Reset for the next line.
-      $outarray = $empty;
-      $last_line = $row['line_record_name'];
+    // Store in the same order as $markerids[], i.e. table allele_byline_idx.
+    foreach ($markerids as $id) {
+      $afreq[$id] = ($aa_sum[$id] + 0.5 * $ab_sum[$id]) / $total_sum[$id];
+      $afreq[$id] = number_format($afreq[$id], 3);
     } 
-    else {
-      // Still collecting alleles for the current germplasm line.
-      $mname = $row['markername'];				
-      if ($lookup[$row['value']] != "--") {
-	// (For missing data, use the default value.)
-	$outarray[$mname] = $lookup[$row['value']];
+    // Read in the allele_byline table.
+    $sql = "select * from allele_byline";
+    $res = mysql_query($sql) or die(mysql_error());
+    while ($row = mysql_fetch_array($res)) {
+      $lineid = $row['line_record_uid'];
+      $line = $row['line_record_name'];
+      $alleles = explode(',', $row['alleles']);
+      for ($i=0; $i<count($alleles); $i++) {
+	if ($alleles[$i] == '' or $alleles[$i] == '--')
+	  // Substitute global frequency for missing values.
+	  $alleles[$i] = $afreq[$markerids[$i]];
+	else
+	  // Translate to numeric score.
+	  $alleles[$i] = $lookup[$alleles[$i]];
+      }
+      $alleles = implode(',', $alleles);
+      // Store in cache table.
+      $sql = "insert into allele_byline_clust values (
+         $lineid, '$line', '$alleles', NOW() )";
+      mysql_query($sql) or die(mysql_error()."<br>Query:<br>$sql");
+    }
+  } // end of if($update)
+
+  $sql = "select marker_uid, marker_name from allele_byline_idx order by marker_uid";
+                $res = mysql_query($sql) or die(mysql_error() . "<br>" . $sql);
+                $i=0;
+                while ($row = mysql_fetch_array($res)) {
+                   $marker_list[$i] = $row[0];
+                   $marker_list_name[$i] = $row[1];
+                   $i++;
+                }
+
+  $markers = $_SESSION['filtered_markers'];
+  foreach ($markers as $temp) {
+    $marker_lookup[$temp] = 1;
+  }
+  // Save the list of marker names to the output file.
+  //$outputheader = trim($outputheader, ",")."\n";
+  $outputheader = '';
+  foreach ($marker_list as $i => $marker_id) {
+    $marker_name = $marker_list_name[$i];
+    if (isset($marker_lookup[$marker_id])) {
+      if ($outputheader == '') {
+         $outputheader .= $marker_name;
+      } else {
+         $outputheader .= $delimiter.$marker_name;
       }
     }
   }
+  $outputheader .= "\n";
+  // Make the filename unique to deal with concurrency.
+  $time = $_GET['time'];
+  if (! file_exists('/tmp/tht')) mkdir('/tmp/tht');
+  $outfile = "/tmp/tht/mrkData.csv".$time;
+  file_put_contents($outfile, $outputheader);
 
-  //Old note from downloads.php.  Still relevant?
-  // //NOTE: there is a problem with the last line logic here. Must fix.
-  //Save data from the last line.
-  $outarray = implode($delimiter,$outarray);
-  file_put_contents($outfile, $last_line.$delimiter.$outarray."\n", FILE_APPEND);
- }
-echo "</div>";
+  //$starttime = time();
+  // Get the alleles for currently selected lines, all genotyped markers.	
+  foreach ($_SESSION['filtered_lines'] as $lineuid) {
+    $sql = "select line_record_name, alleles from allele_byline_clust
+          where line_record_uid = $lineuid";
+    //echo "$sql<br>\n";
+    $res = mysql_query($sql) or die(mysql_error());
+    if ($row = mysql_fetch_array($res)) 
+      $outarray2 = array();
+      $line_name = $row[0];
+      $alleles = $row[1];
+      //echo "$line_name $alleles\n";
+      $outarray = explode(',',$alleles);
+      $i=0;
+      foreach ($outarray as $allele) {
+        $marker_id = $marker_list[$i];
+        if (isset($marker_lookup[$marker_id])) {
+          $outarray2[]=$allele;
+        }
+        $i++;
+      }
+      $outarray = implode($delimiter,$outarray2);  
+      file_put_contents($outfile, $line_name.$delimiter.$outarray."\n", FILE_APPEND);
+    }
+    $elapsed = time() - $starttime;
+    $_SESSION['timmer'] = $elapsed;
+  }
 
-
-echo "</div></div>";
+echo "</div></div></div>";
 
 ?>
