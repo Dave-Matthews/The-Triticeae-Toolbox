5cf1b86563e1e93a213cebd662804aa92496dbbf
Clay Birkett <clb343@cornell.edu>
2013-04-15 08:49:59 -0700

    modify Calculate CSR page to add 3rd wavelength and option for plot scalling

--- curator_data/csr.js
+++ curator_data/csr.js
@@ -1,4 +1,4 @@
-/*global $,Ajax,Element*/
+/*global $,Ajax,Element,window*/
 
 var php_self = document.location.href;
 var title = document.title;
@@ -9,11 +9,39 @@ var w1 = "";
 var w2 = "";
 var w3 = "";
 var smooth = "";
+var xrange = "formula";
 
 function display(option) {
   var uid = option;
   var url = php_self + "?function=display&uid=" + uid;
   document.location = url;
+}
+
+function update_zoom(frm) {
+  if (frm.xrange[0].checked) {
+    xrange = "entire";
+  } else {
+    xrange = "formula";
+  }
+  if (trial.length > 1) {
+  var param = 'trial=' + trial;
+  param += '&W1=' + w1;
+  param += '&W2=' + w2;
+  param += '&W3=' + w3;
+  param += '&formula1=' + formula1;
+  param += '&formula2=' + formula2;
+  param += '&smooth=' + smooth;
+  param += '&xrange=' + xrange;
+  var url = "curator_data/cal_index_check.php";
+  Element.show('spinner');
+  var tmp = new Ajax.Updater($('step2'), url, {method: 'post', postBody: param}, {
+        onComplete : function() {
+            $('step2').show();
+            document.title = title;
+            Element.hide('spinner');
+        }
+    });
+  } 
 }
 
 function display2(option) {
@@ -42,17 +70,40 @@ function update_trial() {
 }
 
 function update_w1() {
-  w1 = document.getElementById("W1").value;
+  var test = document.getElementById("W1").value;
+  if (isNaN(test)) {
+    window.alert("value must be a number");
+    document.getElementById("W1").value = "";
+  } else {
+    w1 = document.getElementById("W1").value;
+  }
 }
 
 function update_w2() {
-  w2 = document.getElementById("W2").value;
+  var test = document.getElementById("W2").value;
+  if (isNaN(test)) {
+    window.alert("value must be a number");
+    document.getElementById("W2").value = "";
+  } else {
+    w2 = document.getElementById("W2").value;
+  }
+}
+
+function update_w3() {
+  var test = document.getElementById("W3").value;
+  if (isNaN(test)) {
+    window.alert("value must be a number");
+    document.getElementById("W3").value = "";
+  } else {
+    w3 = document.getElementById("W3").value;
+  }
 }
 
 function update_f1() {
   var desc = "";
   var e = document.getElementById("formula1");
   formula1 = e.options[e.selectedIndex].value;
+  w3 = "";
   if (formula1 == "NWI1") {
     w2 = 970;
     w1 = 900;
@@ -77,15 +128,27 @@ function update_f1() {
     w2 = 780;
     w1 = 550;
     formula2 = "(W2-W1)/(W1+W2)";
-     desc = "<a target='_blank' href=http://en.wikipedia.org/wiki/Normalized_Difference_Vegetation_Index>Green Normalized Difference Vegetation Index</a>";
+    desc = "<a target='_blank' href=http://en.wikipedia.org/wiki/Normalized_Difference_Vegetation_Index>Green Normalized Difference Vegetation Index</a>";
   } else if (formula1 == "SR") {
     w2 = 900;
     w1 = 680;
     formula2 = "(W2-W1)";
     desc = "Simple Ratio";
+  } else if (formula1 == "OSAVI") {
+    w2 = 800;
+    w1 = 670;
+    formula2 = "(1+0.16)*(W2-W1)/(W2+W1)";
+    desc = "<a target='_blank' href=http://digital.csic.es/bitstream/10261/10635/1/26.pdf>Optimized Soil-Adjusted Vegetation Index</a>";
+  } else if (formula1 == "TCARI") {
+    w3 = 700;
+    w2 = 670;
+    w1 = 550;
+    formula2 = "3*(W3-W2) - 0.2*(W3-W1)";
+    desc = "<a target='_blank' href=http://digital.csic.es/bitstream/10261/10635/1/26.pdf>Transformed Chlorophyll Absorption Index</a>";
   }
   document.getElementById("W1").value = w1;
   document.getElementById("W2").value = w2;
+  document.getElementById("W3").value = w3;
   document.getElementById("formula2").value = formula2;
   document.getElementById("formdesc").innerHTML = desc;
   formula2 = encodeURIComponent(formula2); 
@@ -112,6 +175,7 @@ function cal_index() {
   var param = 'trial=' + trial;
   param += '&W1=' + w1;
   param += '&W2=' + w2;
+  param += '&W3=' + w3;
   param += '&formula1=' + formula1;
   param += '&formula2=' + formula2;
   param += '&smooth=' + smooth;
