3d898feb1c4d02106b1f7e9ba320c1b8bbb4bd17
Clay Birkett <clb343@cornell.edu>
2013-04-17 08:23:21 -0700

    modify GWAS page to provide link to download Kinship matrix

--- gensel.php
+++ gensel.php
@@ -28,6 +28,7 @@ date_default_timezone_set('America/Los_Angeles');
 
 require_once $config['root_dir'].'includes/MIME/Type.php';
 require_once $config['root_dir'].'includes/File_Archive/Archive.php';
+require_once $config['root_dir'].'downloads/marker_filter.php';
 
 // connect to database
 connect();
@@ -704,6 +705,8 @@ class Downloads
         $res = mysql_query($sql) or die(mysql_error());
         $row = mysql_fetch_array($res);
         $phenolabel = $row[0];
+        //$unique_fld = chr(rand(65,80)).chr(rand(65,80)).chr(rand(65,80)).chr(rand(65,80));
+        //mkdir("/tmp/tht/$unique_fld");  it would be better to put all files in directory
         $dir = '/tmp/tht/';
         $filename9 = 'THTdownload_hmp_' . $unique_str. '.txt';
         $filename2 = 'THTdownload_traits_' . $unique_str . '.txt';
@@ -714,6 +717,7 @@ class Downloads
         $filename5 = 'process_error_gwa_' . $unique_str . '.txt';
         $filename6 = 'R_error_gwa_' . $unique_str . '.txt';
         $filename1 = 'THT_result_' . $unique_str . '.csv';
+        $filenameK = 'Kinship_matrix_' . $unique_str . '.csv';
         if(!file_exists($dir.$filename3)){
             $h = fopen($dir.$filename3, "w+");
             $png1 = "png(\"$dir$filename4\", width=800, height=400)\n";
@@ -726,6 +730,7 @@ class Downloads
             $cmd6 = "fileerr <- \"$dir$filename6\"\n";
             $cmd7 = "fileout <- \"$filename1\"\n";
             $cmd8 = "model_opt <- \"$model_opt\"\n";
+            $cmd9 = "fileK <- \"$filenameK\"\n";
             fwrite($h, $png1);
             fwrite($h, $png2);
             fwrite($h, $png3);
@@ -736,6 +741,7 @@ class Downloads
             fwrite($h, $cmd6);
             fwrite($h, $cmd7);
             fwrite($h, $cmd8);
+            fwrite($h, $cmd9);
             fwrite($h, "setwd(\"/tmp/tht/\")\n");
             fclose($h);
         }
@@ -753,6 +759,7 @@ class Downloads
                   print "<img src=\"/tmp/tht/$filename4\" /><br>";
                   print "<a href=/tmp/tht/$filename1 target=\"_blank\" type=\"text/csv\">Export GWAS results to CSV file</a> ";
                   print "with columns for marker name, chromosome, position, marker score<br><br>";
+                  print "<a href=/tmp/tht/$filenameK target=\"_blank\" type=\"text/csv\">Export Kinship matrix</a> ";
         }
         if (file_exists("/tmp/tht/$filename5")) {
            $h = fopen("/tmp/tht/$filename5", "r");
@@ -966,10 +973,10 @@ class Downloads
                 <?php
               
                 if ($training_lines == "") {
-                  $this->calculate_af($lines, $min_maf, $max_missing, $max_miss_line);
+                  calculate_af($lines, $min_maf, $max_missing, $max_miss_line);
                   $lines = $_SESSION['filtered_lines'];
                 } else {
-                  $this->calculate_af($training_lines, $min_maf, $max_missing, $max_miss_line);
+                  calculate_af($training_lines, $min_maf, $max_missing, $max_miss_line);
                   $training_lines = $_SESSION['filtered_lines'];
                 }
                 $markers = $_SESSION['filtered_markers'];
@@ -1583,150 +1590,6 @@ class Downloads
 	    return 0;
 	  }
 	  return ($a < $b) ? -1 : 1;
-	}
-	
-	/**
-	 * display minor allele frequence and missing data using selected lines
-	 * @param array $lines
-	 * @param floats $min_maf
-	 * @param floats $max_missing
-	 */
-	function calculate_af(&$lines, $min_maf, $max_missing, $max_miss_line) {
-	 //calculate allele frequencies using 2D table
-
-	 if (isset($_SESSION['clicked_buttons'])) {
-	   $tmp = count($_SESSION['clicked_buttons']);
-	   $saved_session = $saved_session . ", $tmp markers";
-	   $markers = $_SESSION['clicked_buttons'];
-	   $marker_str = implode(',',$markers);
-	 } else {
-           $markers_filtered = array();
-	   $markers = array();
-	   $marker_str = "";
-	 }
-	 
-	 //get location information for markers
-	 $sql = "select marker_uid, marker_name from allele_byline_idx order by marker_uid";
-	 $res = mysql_query($sql) or die(mysql_error() . "<br>" . $sql);
-	 $i=0;
-	 while ($row = mysql_fetch_array($res)) {
-          $uid = $row[0];
-	  $marker_list[$i] = $row[0];
-          $marker_list_name[$i] = $row[1];
-          $marker_list_loc[$uid] = $i;
-	  $i++;
-	 }
-  
-         //get location information for lines
-         $sql = "select line_record_uid, line_record_name from line_records";
-         $res = mysql_query($sql) or die(mysql_error() . "<br>" . $sql);
-         while ($row = mysql_fetch_array($res)) {
-          $uid = $row[0];
-          $line_list_name[$uid] = $row[1];
-         }
-	
-	 //calculate allele frequence and missing
-         $marker_misscnt = array();
-	 foreach ($lines as $line_record_uid) {
-	   $sql = "select alleles from allele_byline where line_record_uid = $line_record_uid";
-	   $res = mysql_query($sql) or die(mysql_error() . "<br>" . $sql);
-	   if ($row = mysql_fetch_array($res)) {
-	     $alleles = $row[0];
-	     $outarray = explode(',',$alleles);
-             $alleles_mem[$line_record_uid] = $alleles;
-	     $i=0;
-	     foreach ($outarray as $allele) {
-               if ($allele=='AA') { $marker_aacnt[$i]++; }
-               elseif (($allele=='AB') or ($allele=='BA')) { $marker_abcnt[$i]++; }
-               elseif ($allele=='BB') { $marker_bbcnt[$i]++; }
-               elseif (($allele=='--') or ($allele=='')) { $marker_misscnt[$i]++; }
-               else { echo "illegal genotype value $allele for marker $marker_list_name[$i]<br>"; }
-               $i++;
-	     }
-           } else {
-             foreach ($marker_misscnt as $i=>$value) {
-               $marker_misscnt[$i]++;
-             }
-           }
-	 }
-         $i=0;
-	 $num_mark = 0;
-	 $num_maf = $num_miss = $num_removed = 0;
-	 foreach ($marker_list as $marker_uid) {
-	   $total = $marker_aacnt[$i] + $marker_abcnt[$i] + $marker_bbcnt[$i] + $marker_misscnt[$i];
-           $total_af = 2 * ($marker_aacnt[$i] + $marker_abcnt[$i] + $marker_bbcnt[$i]);
-	   if ($total_af > 0) {
-	     $maf = 100 * min((2 * $marker_aacnt[$i] + $marker_abcnt[$i]) /$total, ($marker_abcnt[$i] + 2 * $marker_bbcnt[$i]) / $total);
-	     $miss = 100 * $marker_misscnt[$i]/$total;
-	     if ($maf < $min_maf) $num_maf++;
-	     if ($miss > $max_missing) $num_miss++;
-	     if (($miss > $max_missing) OR ($maf < $min_maf)) {
-               $num_removed++;
-             } else {
-               $markers_filtered[] = $marker_uid;
-             }
-	     $num_mark++;
-	   }
-           $i++; 
-	 }
-         $_SESSION['filtered_markers'] = $markers_filtered;
-         $count = count($markers_filtered);
-
-         //calculate missing from each line
-         foreach ($lines as $line_record_uid) {
-           $alleles = $alleles_mem[$line_record_uid];
-           $outarray = explode(',',$alleles);
-           $line_misscnt[$line_record_uid] = 0;
-           foreach ($markers_filtered as $marker_uid) {
-              $loc = $marker_list_loc[$marker_uid];
-              $allele = $outarray[$loc];
-              if (($allele=='--') or ($allele=='')) { 
-                  $line_misscnt[$line_record_uid]++;
-              }
-           }
-         }
-         $lines_removed = 0; 
-         $lines_removed_name = "";
-         $num_line = 0;
-         foreach ($lines as $line_record_uid) {
-           $total = count($markers_filtered);
-           $miss = 100*$line_misscnt[$line_record_uid]/$total;
-           if ($miss > $max_miss_line) {
-             $lines_removed++;
-             if ($lines_removed_name == "") {
-               $lines_removed_name = $line_list_name[$line_record_uid];
-             } else {
-               $lines_removed_name = $lines_removed_name . ", $line_list_name[$line_record_uid]";
-             }
-           } else {
-             $lines_filtered[] = $line_record_uid;
-           }
-           $num_line++;
-         }
-         $_SESSION['filtered_lines'] = $lines_filtered;
-         $comm = substr($lines_removed_name, 0, 100);
-         $count2 = count($lines_filtered);
-	 
-	  ?>
-        <table>
-        <tr><td>filtered<td>remaining
-	<tr><td><b><?php echo ($num_maf) ?></b><i> markers have a minor allele frequency (MAF) less than </i><b><?php echo ($min_maf) ?></b><i>%
-	<br><b><?php echo ($num_miss) ?></b><i> markers are missing more than </i><b><?php echo ($max_missing) ?></b><i>% of data
-        <td><b><?php echo ("$count") ?></b><i> markers</i> 
-        <?php
-        echo "<tr><td>using<b> $count</b> markers<br>";
-        if ($lines_removed == 1) {
-          echo ("</i><b>$lines_removed") ?></b><i> line is missing more than </i><b><?php echo ($max_miss_line) ?></b><i>% of data</b></i>
-          <?php
-        } else {
-          echo ("</i><b>$lines_removed") ?></b><i> lines are missing more than </i><b><?php echo ($max_miss_line) ?></b><i>% of data</b></i>
-          <?php
-        }
-        if ($lines_removed_name != "") {
-          echo "(<a title=\"$lines_removed_name\">$comm</a>).";
-        }
-        echo "<td><b>$count2</b><i> lines</a>";
-        echo ("</table>");
 	}
 	
 	/**
@@ -2391,7 +2254,7 @@ selected lines</a><br>
 		elseif ($min_maf<0)
 			$min_maf = 0;
 		
-		$this->calculate_af($lines, $min_maf, $max_missing, $max_miss_line);
+		calculate_af($lines, $min_maf, $max_missing, $max_miss_line);
 		
 		?>
 		<table> <tr> <td COLSPAN="3">
@@ -2508,7 +2371,7 @@ selected lines</a><br>
 	 elseif ($min_maf<0)
 	   $min_maf = 0;
 	
-	$this->calculate_af($lines, $min_maf, $max_missing, $max_miss_line);
+	 calculate_af($lines, $min_maf, $max_missing, $max_miss_line);
 	
 	?>
 	<table> <tr> <td COLSPAN="3">
