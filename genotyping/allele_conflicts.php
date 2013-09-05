82d56182456bccc2a3fee0d5d049d2c77ac455a3
Dave Matthews <matthews@greengenes.cit.cornell.edu>
2013-09-05 14:42:03 -0700

    Allow restricting to Currently Selected Lines.  Allow downloading.

--- genotyping/allele_conflicts.php
+++ genotyping/allele_conflicts.php
@@ -1,17 +1,61 @@
 <?php
 require 'config.php';
 include($config['root_dir'].'includes/bootstrap.inc');
-include($config['root_dir'].'theme/admin_header.php');
 connect();
+
+$rest = $_REQUEST[restrict];
+$dl = $_REQUEST[download];
+if (empty($rest))
+  $rest = "No";
+// Show conflicts only for Currently Selected Lines?
+if ($rest == 'Yes') {
+  $lineids = implode(",", $_SESSION['selected_lines']);
+  if (empty($lineids))
+    $lineids = "''";
+  $restriction = "and l.line_record_uid in ($lineids)";
+}
+$query = "select l.line_record_name, m.marker_name, a.alleles, e.trial_code
+from allele_conflicts a, line_records l, markers m, experiments e
+where a.line_record_uid = l.line_record_uid
+  and a.marker_uid = m.marker_uid
+  and a.experiment_uid = e.experiment_uid
+  and a.alleles != '--'
+  $restriction
+order by l.line_record_name, m.marker_name, e.trial_code";
+$result = mysql_query($query) or die(mysql_error());
+
+// Downloading?
+if (!empty($_REQUEST[download])) {
+  header('Content-disposition: attachment;filename=allele_conflicts.csv');
+  print "Line,Marker,Alleles,Experiment\n";
+  while ($row=mysql_fetch_row($result)) {
+    $rowstring = implode(",", $row);
+    print $rowstring."\n";
+  }
+}
+else {
+  include($config['root_dir'].'theme/admin_header.php');
 ?>
 
-<div id="primaryContentContainer">
-  <div id="primaryContent">
+<style type=text/css>
+table td {padding-top:1px; padding-bottom:1px}
+table th {text-align:left}
+</style>
+
   <h1>Allele Conflicts</h1>
   <div class="section">
-  <p>Lines that have been genotyped more than once for the same markers, with different results.
-  Missing values ("--") are excluded.</p>
-
+  <form>
+    Lines that have been genotyped more than once for the same markers, with different results.
+    Missing values ("--") are excluded.<br>
+    Restrict to <a href="<?php echo $config[base_url] ?>pedigree/line_properties.php">currently selected lines</a>? 
+    <input type=submit name=restrict value=Yes> 
+    <input type=submit name=restrict value=No>
+  </form>
+  <form>
+    <input type=hidden name=restrict value=<?php echo $rest ?>>
+    <input type=submit name=download value=Download>
+  </form>
+  <p>
   <table>
     <tr>
       <th>Line</th>
@@ -21,26 +65,16 @@ connect();
     </tr>
 
 <?php
-$query = "select l.line_record_name, m.marker_name, a.alleles, e.trial_code
-from allele_conflicts a, line_records l, markers m, experiments e
-where a.line_record_uid = l.line_record_uid
-  and a.marker_uid = m.marker_uid
-  and a.experiment_uid = e.experiment_uid
-  and a.alleles != '--'
-order by l.line_record_name, m.marker_name, e.trial_code";
-$result = mysql_query($query) or die(mysql_error());
-while ($row=mysql_fetch_row($result)) {
-  print "<tr>";
-  for ($i=0; $i<4; $i++) {
-    print "<td>$row[$i]</td>";
+  while ($row=mysql_fetch_row($result)) {
+    print "<tr>";
+    for ($i=0; $i<4; $i++) 
+      print "<td>$row[$i]</td>";
+    print "</tr>";
   }
-  print "</tr>";
+print "</table>";
+
+print "</div>";
+$footer_div=1;
+include($config['root_dir'].'theme/footer.php'); 
 }
 ?>
-  </table>
-
-
-  </div></div></div>
-  <?php 
-  $footer_div=1;
-include($config['root_dir'].'theme/footer.php'); ?>
