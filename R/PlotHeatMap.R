acdcaf1f5e0a4a5538c10683f8ed0442c3cd467a
Clay Birkett <clb343@cornell.edu>
2013-09-30 08:38:23 -0700

    add scale to heatmap

--- R/PlotHeatMap.R
+++ R/PlotHeatMap.R
@@ -1,5 +1,6 @@
 x <- as.matrix(read.delim("plotMap.txt", header=TRUE, sep="\t"))
-heatmap(x, scale = "none",
+cc <- rainbow(ncol(x), start = 0, end = 0.5)
+heatmap(x, scale = "none",  ColSideColors = cc,
               xlab = "column", ylab =  "row",
               Rowv = NA, Colv = NA )
 dev.off()
