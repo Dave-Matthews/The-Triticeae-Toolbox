3a8a107fd7694ea613187fdb0312f865544958f8
Clay Birkett <clb343@cornell.edu>
2013-11-14 07:53:59 -0800

    add page for creating marker panel

---
+++ genotyping/403.php
@@ -0,0 +1,24 @@
+<?php
+include("includes/bootstrap.inc");
+connect();
+include("theme/normal_header.php");
+?>
+
+<div id="primaryContentContainer">
+	<div id="primaryContent">
+  		<div class="box">
+
+			<h2>Access Denied</h2>
+
+			<div class="boxContent">
+	
+			   <p>Sorry, but you must be <a href="login.php">logged in</a> to view that page</p>
+
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+
+
+<?php include("theme/footer.php"); ?>
