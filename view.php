f976b8a49595504637d44d9990cd5aee7d48fcdb
Clay Birkett <clb343@cornell.edu>
2013-12-20 13:20:13 -0800

    improve error handling

--- view.php
+++ view.php
@@ -1,69 +1,60 @@
-<?php 
-	include("includes/bootstrap.inc");
-	connect();
+<?php
+/**
+ * View database entry
+ * 
+ * PHP version 5.3
+ * Prototype version 1.5.0
+ * 
+ * @category PHP
+ * @package  T3
+ * @author   Clay Birkett <clb343@cornell.edu>
+ * @license  http://triticeaetoolbox.org/wheat/docs/LICENSE Berkeley-based
+ * @version  GIT: 2
+ * @link     http://triticeaetoolbox.org/wheat/view.php
+ * 
+ */
 
-	include("theme/normal_header.php");
+require "includes/bootstrap.inc";
+connect();
+require "theme/normal_header.php";
 
-	// Will take both uids and names
-	if(isset($_REQUEST['name']) && !isset($_REQUEST['uid']))
-		$_REQUEST['uid'] = $_REQUEST['name'];
-	
-	if (strpos($_REQUEST['uid'], "=", strlen($_REQUEST['uid'])-1) !== FALSE) {
-		$_REQUEST['uid'] = base64_decode($_REQUEST['uid']);
-	}
+// Will take both uids and names
+if (isset($_REQUEST['name']) && !isset($_REQUEST['uid'])) {
+    $_REQUEST['uid'] = $_REQUEST['name'];
+}
+if (isset($_REQUEST['uid'])) {
+    if (strpos($_REQUEST['uid'], "=", strlen($_REQUEST['uid'])-1) !== false) {
+        $_REQUEST['uid'] = base64_decode($_REQUEST['uid']);
+    }
+}
+
+if ( ($record = isRecord($_REQUEST['table'], $_REQUEST['uid'])) !== false) { 
+    $row = mysql_fetch_assoc($record);
+    $pkey = get_pkey($_REQUEST['table']);
+    $name = get_unique_name($_REQUEST['table']);		   
+    ?>
+
+    <h1>
+    <?php echo beautifulTableName($_REQUEST['table'], 0) . " " . $row[$name] ?></h1>
+    <div class="boxContent">
+
+    <?php 
+    $func = "show_" . $_REQUEST['table'];
+    // Is there a custom function for that table in includes/general.inc
+    // or includes/pedigree.inc?  examples:
+    //  line_records = includes/pedigree.inc/show_line_records()
+    //  markers = includes/general.inc/show_markers()
+    //  breeding_programs = includes/general.inc/show_breeding_programs()
+    if (function_exists($func)) {
+        call_user_func($func, $row[$pkey]);
+    } else {
+        // Default to raw table dump using includes/general.inc:show_general().
+        show_general($_REQUEST['table'], $row[$pkey]);
+    }
+    echo "</div>";
+} else {
+    error(1, "No Record Found"); 
+}
+echo "</div>";
+require "theme/footer.php";
 ?>
-
-
-<!--<div id="primaryContentContainer">
-	<div id="primaryContent">-->
-
-	<?php if( ($record = isRecord($_REQUEST['table'], $_REQUEST['uid'])) !== FALSE) { 
-		   $row = mysql_fetch_assoc($record);
-		   $pkey = get_pkey($_REQUEST['table']);
-		   $name = get_unique_name($_REQUEST['table']);		   ?>
-
-		<!--<div class="box">-->
-<h1><?php echo beautifulTableName($_REQUEST['table'], 0) . " " . $row[$name] ?></h1>
-<div class="boxContent">
-
-		<?php 
-
-			//Example:  Line records = show_line_records();
-			//	    Breeding Programs = show_breeding_programs();
-
-			$func = "show_" . $_REQUEST['table'];
-
-			// Is there a custom function for that table, see examples above
-			if(function_exists($func))
-				call_user_func($func, $row[$pkey]);
-	
-			// default to raw table dump.
-			else {
-
-				show_general($_REQUEST['table'], $row[$pkey]);
-
-				/*
-				echo "<table class=\"tableclass1\" cellspacing=0 cellpadding=0>\n";				
-				foreach($row as $k=>$v) {
-					echo "\t<tr>\n";
-					echo "\t\t<td><strong>". ucfirst(str_replace("_", " ", $k)) . "</strong></td>\n";
-					echo "\t\t<td>$v</td>\n";
-					echo "\t</tr>\n";
-				}
-				echo "</table>";
-				*/
-			}
-
-		 ?>
-
-</div>
-		<!--</div>-->
-
-
-
-	<?php } else { error(1, "No Record Found"); } ?>
-	<!--</div>
-</div>-->
-</div>
-
-<?php include("theme/footer.php");?>
