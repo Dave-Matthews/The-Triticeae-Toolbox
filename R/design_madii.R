a08a002ba601da3313eee65f978bc50910e404ca
Clay Birkett <clb343@cornell.edu>
2014-05-19 07:23:19 -0700

    correct bugs in madii design

--- R/design_madii.R
+++ R/design_madii.R
@@ -1,5 +1,8 @@
 source(common_code)
 
 message("MADII design")
-outdesign<- MADIIdgn(num.entries=90, num.rows=9, num.cols=NULL, num.sec.chk=3, designID="tester1", annoy=T)
+
+num.entries <- length(trt)
+num.checks <- length(trt2)
+outdesign<- MADIIdgn(entries=trt, num.entries, num.rows=num_row, num.cols=num_col, chk.names=trt2, num.sec.chk=num.checks, designID="tester1", annoy=T)
 
