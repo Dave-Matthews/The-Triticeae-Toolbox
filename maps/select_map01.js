4839aa6aa5dcbd236dcc9c4a16d015767a3ef3f7
Clay Birkett <clb343@cornell.edu>
2014-06-02 07:17:32 -0700

    make calculation of markes in maps optional

--- maps/select_map01.js
+++ maps/select_map01.js
@@ -1,10 +1,12 @@
-/*global, alert, jQuery, $ */
+/*global, alert, jQuery, $, Element */
 
 var map = 1;
+var markersInMap = null;
 var php_self = document.location.href;
 $.noConflict(); //when prototype.js is removed then this is not necessary
 
 function save_map() {
+  Element.hide('spinner');
   var i;
   for (i=0; i<document.myForm.map.length; i++) {
       if (document.myForm.map[i].checked===true) {
@@ -16,7 +18,7 @@ function save_map() {
     url: php_self,
     data: "map=" + map + "&function=Save",
     success: function(data, textStatus) {
-        jQuery("#step2").html(data);
+        jQuery("#step3").html(data);
     },
     error: function() {
         alert('Error in selecting design type');
@@ -25,16 +27,15 @@ function save_map() {
 }
 
 function load_markersInMap() {
-  document.getElementById("step3").innerHTML = "Calculating portion of markers in each map.";
-  jQuery.ajax({
+  Element.show('spinner');
+  document.getElementById("step4").innerHTML = "Calculating portion of markers in each map.";
+  markersInMap = jQuery.ajax({
     type: "GET",
     url: php_self,
     data: "function=Markers",
     success: function(data, textStatus) {
-        jQuery("#step3").html(data);
-    },
-    error: function() {
-        alert('Error in selecting design type');
-      }
+        jQuery("#step4").html(data);
+        Element.hide('spinner');
+    }
   });
 }
