fe796f444f37aa7e73a001e39f064711a65f7e89
Clay Birkett <clb343@cornell.edu>
2014-09-02 10:31:49 -0700

    add "number of markers" to table

--- genotyping/genotype_selection.php
+++ genotyping/genotype_selection.php
@@ -48,8 +48,11 @@ function check_session() {
     
 function display_list() {
     global $mysqli;
-    print "<h2>Genotype experiments ordered by creation date</h2>\n";
-    print "<table border=0>"; print "<tr><td>Trial Code<td>year<td>description\n";
+    ?>
+    <h2>Genotype experiments ordered by creation date</h2>
+    <table border=0>
+    <tr><td>Trial Code<td>year<td>number of<br>markers<td>description
+    <?php
     $sql = "select CAPdata_programs_uid, data_program_name from CAPdata_programs";
     $res = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
     while ($row = mysqli_fetch_row($res)) {
@@ -57,7 +60,8 @@ function display_list() {
         $name = $row[1];
         $cap_prog_name[$uid] = $name;
     }
-    $sql = "select CAPdata_programs_uid, trial_code, experiment_short_name, experiment_year, experiment_desc_name from experiments, experiment_types
+    $sql = "select experiment_uid, CAPdata_programs_uid, trial_code, experiment_short_name, experiment_year, experiment_desc_name
+        from experiments, experiment_types
         where experiments.experiment_type_uid = experiment_types.experiment_type_uid and experiment_types.experiment_type_name = 'genotype'";
     if (!authenticate(array(USER_TYPE_PARTICIPANT,
                                         USER_TYPE_CURATOR,
@@ -66,11 +70,17 @@ function display_list() {
     $sql .= " order by experiments.experiment_year desc";
     $res = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
     while ($row = mysqli_fetch_row($res)) {
-        $trial_code = $row[1];
-        $short_name = $row[2];
-        $date = $row[3];
-        $desc = $row[4];
-        print "<tr><td><a href=display_genotype.php?trial_code=$trial_code>$trial_code</a><td>$date<td>$desc\n";
+        $experiment_uid = $row[0];
+        $trial_code = $row[2];
+        $short_name = $row[3];
+        $date = $row[4];
+        $desc = $row[5];
+        $sql = "select count(marker_uid) from allele_frequencies where experiment_uid = $experiment_uid";
+        $res2 = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
+        if ($row2 = mysqli_fetch_row($res2)) {
+            $count = number_format($row2[0]);
+        }
+        print "<tr><td><a href=display_genotype.php?trial_code=$trial_code>$trial_code</a><td>$date<td style=\"text-align:right\">$count<td>$desc\n";
     }
     echo "</table></div>";
 }
