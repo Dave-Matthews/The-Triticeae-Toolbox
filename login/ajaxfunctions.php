4ec81466f89809073e4721e391ff335676eec987
Dave Matthews <matthews@greengenes.cit.cornell.edu>
2014-09-16 13:06:00 -0700

    Add AJAX for public line panel descriptions in login/line_panels.php.

---
+++ login/ajaxfunctions.php
@@ -0,0 +1,23 @@
+<?php
+// login/ajaxfunctions.php, dem jul2013
+// PHP functions that are called via html GET by javascript functions in this directory.
+
+require 'config.php';
+include("$config[root_dir]/includes/bootstrap_curator.inc");
+$link = connect();
+
+$functioncall = $_GET[func];
+unset($_GET['func']);   //removing function name                                                            
+call_user_func($functioncall, $_GET);
+// Close mysql connection to prevent overloading.
+mysql_close($link);
+
+
+// called by line_panels.php pickpanel()
+function dispDesc($args) {
+  $panelid = $args[panelid];
+  $desc = mysql_grab("select comment from linepanels where linepanels_uid = $panelid");
+  echo $desc;
+}
+
+?>
