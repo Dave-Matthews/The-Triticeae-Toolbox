d063469039e513c22d27980224d0b529c2696a81
Clay Birkett <clb343@cornell.edu>
2014-10-01 13:22:57 -0700

    another try at fixing timeout problem

--- maps/select_map.php
+++ maps/select_map.php
@@ -16,6 +16,7 @@ namespace T3;
 require_once 'config.php';
 require_once $config['root_dir'].'includes/bootstrap.inc';
 connect();
+$mysqli = connecti();
 require_once 'select_map_class.php';
 
 new Maps($_GET['function']);
