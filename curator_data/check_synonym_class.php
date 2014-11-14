1c3afffa03e5a41cc192d13d66e935ed56f3fb00
Clay Birkett <clb343@cornell.edu>
2014-11-14 07:37:14 -0800

    debug tcap machine background

--- curator_data/check_synonym_class.php
+++ curator_data/check_synonym_class.php
@@ -3,9 +3,10 @@
 /**
  * Takes a needle and haystack (just like in_array()) and does a wildcard search on it's values.
  *
- * @param    string        $string        Needle to find
- * @param    array        $array        Haystack to look through
- * @result    array                    Returns the elements that the $string was found in
+ * @param string $string Needle to find
+ * @param array  $array  Haystack to look through
+ *
+ * @result array Returns the elements that the $string was found in
 */
 function find($string, $array = array ())
 {
@@ -33,7 +34,7 @@ function error($type, $text)
     }
 }
 
-/* check how many processes are running */
+/** check how many processes are running */
 function isRunning($pidList)
 {
     $count = 0;
@@ -47,7 +48,7 @@ function isRunning($pidList)
     return $count;
 }
  
-/* break query into files of 1000 to improve performance
+/** break query into files of 1000 to improve performance
  * use word size of half the shortest marker to find all matches
  * detect number of processors
  */
@@ -90,8 +91,8 @@ function typeBlastRun($infile)
             $pidList[$count_file] = rtrim($tmp);
             echo "$count2\t$pidList[$count_file] running BLAST on $count queries";
             if (isRunning($pidList) > $numCpus) {
-                sleep(20);
                 echo "\twaiting 20 seconds for free processor\n";
+                sleep(20);
             } else {
                 echo "\n";
             }
@@ -109,6 +110,8 @@ function typeBlastRun($infile)
     $pidList[$count_file] = rtrim($tmp);
     echo "$count2\t$pidList[$count_file] running BLAST on $count queries\n";
     fclose($fh);
+    echo "waiting 5 seconds for processes to start\n";
+    sleep(5);
     $running = 1;
     while ($running) {
         $count = isRunning($pidList);
