882faf089c509a8b557d31ac95abc6352f13fe2e
Clay Birkett <clb343@cornell.edu>
2015-02-17 13:18:27 -0800

    clean up presentation of feedback form

--- feedback2.php
+++ feedback2.php
@@ -7,15 +7,7 @@ require_once($config['root_dir'] . 'theme/normal_header.php');
 ini_set('magic_quotes_gpc', '0');
 
 require_once $config['root_dir'] . 'securimage/securimage.php';
-?>
 
-<h1>Website Error</h1>
-
-The website has encountered an error.<br>
-Help us to improve the website but submitting an explanation of what you where doing when the error occurred.
-<p>
-
-<?php
 function feedbackForm($name = '', $email = '', $feedback = '')
 {
     global $mysqli;
@@ -74,16 +66,20 @@ $capcha_pass = $securimage->check($_POST['captcha_code']);
 //if ($us_feedback && $capcha_pass) {
 if ($us_feedback && $us_email && $capcha_pass) {
     send_email_from(setting('feedbackmail'), 'T3 Feedback', $us_email,
-"User's reported name: $us_name
-User's reported email: $us_email
+    "User's reported name: $us_name
+    User's reported email: $us_email
 
-Feedback:
-$us_feedback"
-    );
+    Feedback:
+    $us_feedback");
 
-    echo "<h3>Thank you for your feedback. It has been sent to the T3 curators.</h3>";
-}
-elseif ($_POST) {
+    echo "<h2>Thank you for your feedback. It has been sent to the T3 curators.</h2>";
+} elseif ($_POST) {
+    ?>
+    <h1>Website Error</h1>
+    The website has encountered an error.<br>
+    Help us to improve the website but submitting an explanation of what you where doing when the error occurred.
+    <p>
+    <?php
     if (!$capcha_pass) {
         echo "<h3 style='color: red'>Please enter the CAPTCHA code</h3>";
     }
@@ -93,7 +89,15 @@ elseif ($_POST) {
     if (!$us_feedback) {
         echo "<h3 style='color: red'>No message entered.</h3>";
     }
+    echo feedbackForm($us_name, $us_email, $us_feedback);
+} else {
+    ?>
+    <h1>Website Error</h1>
+    The website has encountered an error.<br>
+    Help us to improve the website but submitting an explanation of what you where doing when the error occurred.
+    <p>
+    <?php
+    echo feedbackForm($us_name, $us_email, $us_feedback);
 }
-echo feedbackForm($us_name, $us_email, $us_feedback);
 
-require_once($config['root_dir'] . 'theme/footer.php');
+require_once $config['root_dir'] . 'theme/footer.php';
