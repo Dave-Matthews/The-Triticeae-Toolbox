a0960e0a3c2a7bc4ab1a2f8629e1b8c8eb46c6da
Clay Birkett <clb343@cornell.edu>
2015-02-23 05:45:13 -0800

    make sql queries safe

--- pedigree/show_pedigree.php
+++ pedigree/show_pedigree.php
@@ -3,48 +3,45 @@ require 'config.php';
 /*
  * Logged in page initialization
  */
-include($config['root_dir'].'includes/bootstrap.inc');
-connect();
+include $config['root_dir'].'includes/bootstrap.inc';
+$mysqli = connecti();
 
 
-if(isset($_GET['line']) && ($_GET['line'] != "")) {
-	$safe = mysql_real_escape_string($_GET['line']);
+if (isset($_GET['line']) && ($_GET['line'] != "")) {
+    $safe = mysqli_real_escape_string($mysqli, $_GET['line']);
 
-	//start output buffering, capture any errors here.
-	ob_start();
+    //start output buffering, capture any errors here.
+    ob_start();
 
-	//if $number is FALSE then $safe was not a name, but a number
-	if( ($number = getPedigreeId($safe)) === FALSE)
-		$number = $safe;
+    //if $number is FALSE then $safe was not a name, but a number
+    if (($number = getPedigreeId($safe)) === false) {
+        $number = $safe;
+    }
+    //end output buffering and clean out any errors.
+    ob_end_clean();
 
-	//end output buffering and clean out any errors.
-	ob_end_clean();
+    header("Location: ../view.php?table=line_records&uid=$number");
 
-	header("Location: ../view.php?table=line_records&uid=$number");
-
-
-	/*	echo "<h2>View Line Record " . getAccessionName($number) . "</h2>";
-	showRecord($number); */
-}
-
-else {
-
-include($config['root_dir'].'theme/normal_header.php');
+    /*  echo "<h2>View Line Record " . getAccessionName($number) . "</h2>";
+    showRecord($number); */
+} else {
+    include $config['root_dir'].'theme/normal_header.php';
 
 /*******************************/
 
-?>
-<h1>Browse Line Records</h1>
-<div class="section">
-<p>
-<?php
-	if(isset($_GET['start']))
-		showLines($_SERVER['PHP_SELF'], $_GET['start']);
-	else
-		showLines($_SERVER['PHP_SELF']);
+    ?>
+    <h1>Browse Line Records</h1>
+    <div class="section">
+    <p>
+    <?php
+    if (isset($_GET['start'])) {
+        showLines($_SERVER['PHP_SELF'], $_GET['start']);
+    } else {
+        showLines($_SERVER['PHP_SELF']);
+    }
 }
 ?>
 </p>
 </div>
 </div>
-<?php include($config['root_dir'].'theme/footer.php');?>
+<?php include $config['root_dir'].'theme/footer.php';
