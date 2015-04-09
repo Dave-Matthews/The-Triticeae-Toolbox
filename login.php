92f518f6a84e7e0fba9c11e0aff15a608a62071e
Clay Birkett <clb343@cornell.edu>
2015-04-09 08:20:58 -0700

    all directories should have index.php page

--- login.php
+++ login.php
@@ -14,6 +14,7 @@
 
 
 session_start();
+session_regenerate_id();
 $root = "//" . $_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 $config['base_url'] = "$root";
