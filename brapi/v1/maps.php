263e418b13952b892c2b5e679db3a7be36bbdfdd
Clay Birkett <clb343@cornell.edu>
2015-05-07 12:09:04 -0700

    add count of markers in each set

--- brapi/v1/maps.php
+++ brapi/v1/maps.php
@@ -21,26 +21,31 @@ if (isset($_GET['uid'])) {
 }
 header("Content-Type: application/json");
 if ($action == "list") {
-    $sql = "select mapset_uid, mapset_name, species, map_type, map_unit, published_on, comments
-    from mapset";
+    $sql = "select count(*), mapset.mapset_uid, mapset_name, species, map_type, map_unit, published_on, comments
+    from mapset, markers_in_maps as mim, map
+    WHERE mim.map_uid = map.map_uid
+    AND map.mapset_uid = mapset.mapset_uid
+    GROUP BY mapset.mapset_uid";
     $res = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli) . "<br>$sql");
     while ($row = mysqli_fetch_row($res)) {
-        $temp["uid"] = $row[0];
-        $temp["name"] = $row[1];
-        $temp["species"] = $row[2];
-        $temp["map_type"] = $row[3];
-        $temp["unit"] = $row[4];
-        $temp["published"] = $row[5];
-        $temp["comments"] = $row[6];
+        $temp["mapId"] = $row[1];
+        $temp["name"] = $row[2];
+        $temp["species"] = $row[3];
+        $temp["type"] = $row[4];
+        $temp["unit"] = $row[5];
+        $temp["publishedDate"] = $row[6];
+        $temp["count"] = $row[0];
+        $temp["comments"] = $row[7];
         $results[] = $temp;
     }
     $return = json_encode($results);
     echo "$return";
 } elseif ($uid != "") {
     $pos = 1;
-    $sql = "select marker_uid, start_position, chromosome, arm
-        from markers_in_maps
-        WHERE map_uid = $uid";
+    $sql = "select markers.marker_uid, markers.marker_name, start_position, chromosome, arm
+        from markers_in_maps, markers
+        where markers_in_maps.marker_uid = markers.marker_uid
+        AND map_uid = $uid";
     $res = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli) . "<br>$sql");
     while ($row = mysqli_fetch_row($res)) {
         $temp["markerID"]= $row[0];
