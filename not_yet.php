26be58c5ce1be3aa4ad98ffbc6262747b3a0e952
Dave Matthews <matthews@greengenes.cit.cornell.edu>
2015-08-20 11:17:53 -0700

    Testing

--- not_yet.php
+++ not_yet.php
@@ -18,6 +18,19 @@ connect();
 /* echo "SESSION:"; */
 /* print_h($_SESSION); */
 
+$browser = $_SERVER['HTTP_USER_AGENT'];
+echo "browser = $browser<P>";
+/*
+Firefox: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:39.0) Gecko/20100101 Firefox/39.0
+         Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0
+Chrome:  Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36
+         Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36
+Safari:  Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/7.1.7 Safari/537.85.16
+IE9:     Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)
+ */
+
+echo "JOE";
+
 $footer_div=1;
 include($config['root_dir'].'theme/footer.php'); 
 ?>
