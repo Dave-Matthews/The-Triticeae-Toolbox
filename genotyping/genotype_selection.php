e855a5e4df9eee30012bde5373d79b956b76a3f3
Clay Birkett <clb343@cornell.edu>
2016-02-22 06:41:57 -0800

    fix trial code link

--- genotyping/genotype_selection.php
+++ genotyping/genotype_selection.php
@@ -4,27 +4,23 @@
  *
  * PHP version 5.3
  *
- * @category PHP
- * @package  T3
- * @author   Clay Birkett <cbirkett@gmail.com>
- * @license  http://triticeaetoolbox.org/wheat/docs/LICENSE Berkeley-based
- * @version  GIT: 12/12/2011
- * @link     http://triticeaetoolbox.org/wheat/t3_report.php
- *
+ * @author  Clay Birkett <clb343@cornell.edu>
+ * @license http://triticeaetoolbox.org/wheat/docs/LICENSE Berkeley-based
+ * @link    http://triticeaetoolbox.org/wheat/t3_report.php
  */
 
 require 'config.php';
 require $config['root_dir'].'includes/bootstrap.inc';
 
-$mysql = connect();
 $mysqli = connecti();
 
-include $config['root_dir'].'theme/normal_header.php';
+require $config['root_dir'].'theme/normal_header.php';
 check_session();
 display_list();
-include($config['root_dir'].'theme/footer.php');
+require $config['root_dir'].'theme/footer.php';
 
-function check_session() {
+function check_session()
+{
     global $mysqli;
     if (!isset($_SESSION['geno_exps'])) {
         return;
@@ -35,18 +31,19 @@ function check_session() {
     foreach ($selected as $uid) {
         $sql = $sql = "select trial_code, experiment_year, experiment_desc_name from experiments
             where experiment_uid = $uid";
-        $res = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
+        $res = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
         while ($row = mysqli_fetch_row($res)) {
             $trial_code = $row[0];
             $date = $row[1];
             $desc = $row[2];
-            print "<tr><td><a href=display_genotype.php?trial_code=$trial_code>$trial_code</a><td>$date<td>$desc\n";
+            print "<tr><td><a href='".$config['base_url']."display_genotype.php?trial_code=$trial_code'>$trial_code</a><td>$date<td>$desc\n";
         }
     }
     echo "</table><br><br>";
 } 
     
-function display_list() {
+function display_list()
+{
     global $mysqli;
     ?>
     <h2>Genotype experiments ordered by creation date</h2>
@@ -54,7 +51,7 @@ function display_list() {
     <tr><td>Trial Code<td>year<td>number of<br>markers<td>description
     <?php
     $sql = "select CAPdata_programs_uid, data_program_name from CAPdata_programs";
-    $res = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
+    $res = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
     while ($row = mysqli_fetch_row($res)) {
         $uid = $row[0];
         $name = $row[1];
@@ -63,12 +60,11 @@ function display_list() {
     $sql = "select experiment_uid, CAPdata_programs_uid, trial_code, experiment_short_name, experiment_year, experiment_desc_name
         from experiments, experiment_types
         where experiments.experiment_type_uid = experiment_types.experiment_type_uid and experiment_types.experiment_type_name = 'genotype'";
-    if (!authenticate(array(USER_TYPE_PARTICIPANT,
-                                        USER_TYPE_CURATOR,
-                                        USER_TYPE_ADMINISTRATOR)))
-                        $sql .= " and data_public_flag > 0";
+    if (!authenticate(array(USER_TYPE_PARTICIPANT, USER_TYPE_CURATOR, USER_TYPE_ADMINISTRATOR))) {
+        $sql .= " and data_public_flag > 0";
+    }
     $sql .= " order by experiments.experiment_year desc";
-    $res = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
+    $res = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
     while ($row = mysqli_fetch_row($res)) {
         $experiment_uid = $row[0];
         $trial_code = $row[2];
@@ -76,11 +72,11 @@ function display_list() {
         $date = $row[4];
         $desc = $row[5];
         $sql = "select count(marker_uid) from allele_frequencies where experiment_uid = $experiment_uid";
-        $res2 = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
+        $res2 = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
         if ($row2 = mysqli_fetch_row($res2)) {
             $count = number_format($row2[0]);
         }
-        print "<tr><td><a href=display_genotype.php?trial_code=$trial_code>$trial_code</a><td>$date<td style=\"text-align:right\">$count<td>$desc\n";
+        print "<tr><td><a href='".$config['base_url']."display_genotype.php?trial_code=$trial_code'>$trial_code</a><td>$date<td style=\"text-align:right\">$count<td>$desc\n";
     }
     echo "</table></div>";
 }
