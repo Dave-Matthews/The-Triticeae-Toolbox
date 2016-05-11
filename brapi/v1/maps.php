4c14aac0e19443a3ad9926c372a7ef36645ed495
Clay Birkett <clb343@cornell.edu>
2016-05-11 07:47:30 -0700

    update to latest BRAPI

--- brapi/v1/maps.php
+++ brapi/v1/maps.php
@@ -62,32 +62,46 @@ if ($action == "list") {
         if (preg_match("/0000-00-00/", $temp["publishedDate"])) {
             unset($temp["publishedDate"]);
         } else {
-	    $timestamp = strtotime($temp["publishedDate"]);
-	    // Handle missing values.
-	    if ($timestamp == 0) {
-	        unset($temp["publishedDate"]);
+            $timestamp = strtotime($temp["publishedDate"]);
+            // Handle missing values.
+            if ($timestamp == 0) {
+                unset($temp["publishedDate"]);
             } else {
-	        $temp['publishedDate'] = date("Y-m-d", $timestamp);
+                $temp['publishedDate'] = date("Y-m-d", $timestamp);
             }
         }
-      $temp["markerCount"] = (integer) $row[0];
-      $sql = "select count(distinct(chromosome)) from markers_in_maps, map
+        $temp["markerCount"] = (integer) $row[0];
+        $sql = "select count(distinct(chromosome)) from markers_in_maps, map
         where map.map_uid = markers_in_maps.map_uid
         and mapset_uid = $uid";
-      $res2 = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli) . "<br>$sql");
-      if ($row2 = mysqli_fetch_row($res2)) {
-	$temp["linkageGroupCount"] = (integer) $row2[0];
-      } else {
-	$temp["linkageGroupCount"] = "Error";
-      }
-      $temp["comments"] = $row[7];
-      $linearray['results']['data'][] = $temp;
+        $res2 = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+        if ($row2 = mysqli_fetch_row($res2)) {
+            $temp["linkageGroupCount"] = (integer) $row2[0];
+        } else {
+            $temp["linkageGroupCount"] = "Error";
+        }
+        $temp["comments"] = $row[7];
+        $linearray['result']['data'][] = $temp;
     }
     $return = json_encode($linearray);
     echo "$return";
 } elseif ($uid != "") {
+    $linearray['metadata']['pagination'] = $pageList;
+    $linearray['metadata']['status'] = null;
+    //first query all data
+    $sql = "select markers.marker_uid, markers.marker_name, start_position, chromosome, arm
+        from markers_in_maps, markers, map
+        where markers_in_maps.marker_uid = markers.marker_uid
+        AND map.map_uid = markers_in_maps.map_uid
+        AND mapset_uid = $uid";
+    $res = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli) . "<br>$sql");
+    $num_rows = mysqli_num_rows($res);
+    $tot_pag = ceil($num_rows / $pageSize);
+    $pageList = array( "pageSize" => $pageSize, "currentPage" => 1, "totalCount" => $num_rows, "totalPages" => $tot_pag );
+    $linearray['metadata']['pagination'] = $pageList;
+
     $sql = "select mapset_name, species, map_unit from mapset where mapset_uid = $uid";
-    $res = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli) . "<br>$sql");
+    $res = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
     if ($row = mysqli_fetch_row($res)) {
         $results["name"] = $row[0];
         $results["type"] = $row[1];
@@ -98,19 +112,17 @@ if ($action == "list") {
         where markers_in_maps.marker_uid = markers.marker_uid
         AND map.map_uid = markers_in_maps.map_uid
         AND mapset_uid = $uid";
-    $res = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli) . "<br>$sql");
+    $res = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
     while ($row = mysqli_fetch_row($res)) {
-      $temp2["markerID"]= (integer) $row[0];
+      $temp2["markerId"]= (integer) $row[0];
       $temp2["markerName"] = $row[1];
       $temp2["location"] = $row[2];
-      $temp2["chromosome"] = $row[3];
-      $entries[] = $temp2;
+      $temp2["linkageGroup"] = $row[3];
+      $linearray['result']['data'][] = $temp2;
     }
-    $results["entries"] = $entries;
 
-    $return = json_encode($results);
+    $return = json_encode($linearray);
     echo "$return";
 } else {
     echo "Error: missing experiment id<br>\n";
 }
-?>
