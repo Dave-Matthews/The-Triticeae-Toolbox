a4a4778d0dd390987b852bd1495c6456916e8862
Clay Birkett <clb343@cornell.edu>
2016-06-02 07:33:34 -0700

    allow more memory

--- maps/select_map.php
+++ maps/select_map.php
@@ -16,6 +16,7 @@ require_once 'config.php';
 require_once $config['root_dir'].'includes/bootstrap.inc';
 $mysqli = connecti();
 set_time_limit(120);
+ini_set('memory_limit', '4G');
 require_once 'select_map_class.php';
 
 new Maps($_GET['function']);
