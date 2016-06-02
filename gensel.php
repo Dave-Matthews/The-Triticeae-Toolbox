e8a3dc3bea69281b68370cca1fc4293654199eb4
Clay Birkett <clb343@cornell.edu>
2016-06-02 06:35:00 -0700

    code clean up

--- gensel.php
+++ gensel.php
@@ -143,7 +143,7 @@ class Downloads
         </style>
         <link rel="stylesheet" href="//code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css">
         <script type="text/javascript" src="downloads/download_gs03.js"></script>
-        <script type="text/javascript" src="downloads/downloadsjq02.js"></script>
+        <script type="text/javascript" src="downloads/downloadsjq03.js"></script>
         <div id="title">
         <?php
         $phenotype = "";
