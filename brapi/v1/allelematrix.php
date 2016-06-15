f045922294adc98a009199d0de8e6b23909eec9d
Clay Birkett <clb343@cornell.edu>
2016-06-15 06:53:40 -0700

    fix to match BRAPI

--- brapi/v1/allelematrix.php
+++ brapi/v1/allelematrix.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 if (isset($_GET['pageSize'])) {
     $pageSize = $_GET['pageSize'];
 } else {
-    $pageSize = 100;
+    $pageSize = 1000;
 }
 if (isset($_GET['page'])) {
     $currentPage = $_GET['page'];
@@ -25,6 +25,16 @@ if (isset($_GET['page'])) {
 }
 
 header("Content-Type: application/json");
+
+function dieNice($msg)
+{
+    global $results;
+    $results['metadata']['pagination'] = null;
+    $results['metadata']['status'][] = array("code" => "SQL", "message" => "SQL Error: $msg");
+    $results['result'] = null;
+    $return = json_encode($results);
+    die("$return");
+}
 
 if (isset($_GET['markerprofileDbId'])) {
     //list of markerprofileDbId can be in either format
@@ -46,26 +56,17 @@ if (isset($_GET['markerprofileDbId'])) {
     foreach ($profile_list as $item) {
         if (preg_match("/(\d+)_(\d+)/", $item, $match)) {
             $exp_ary[] = $match[2];
+        } else {
+            $results['metadata']['status'][] = array("code" => "parm", "message" => "Error: invalid format of marker profile id $item");
+            continue;
         }
     }
     $exp_lst = implode(",", $exp_ary);
-    $sql = "select max(count) from allele_byline_exp where experiment_uid IN ($exp_lst)";
-    $res = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
-    if ($row = mysqli_fetch_row($res)) {
-        $num_rows = $row[0];
-    } else {
-        die("Error: experiment not found\n");
-    }
-
-    $sql = "select alleles from allele_byline_exp where experiment_uid IN ($exp_lst)";
-    if ($currentPage == 1) {
-        $sql .= " limit $pageSize";
-    } else {
-        $offset = ($currentPage - 1) * $pageSize;
-        if ($offset < 1) {
-            $offset = 1;
-        }
-        $sql .= " limit $offset, $pageSize";
+    $num_rows = 0;
+    $sql = "select count from allele_byline_exp where experiment_uid IN ($exp_lst)";
+    $res = mysqli_query($mysqli, $sql) or dieNice("invalid experiment_uid");
+    while ($row = mysqli_fetch_row($res)) {
+        $num_rows += $row[0];
     }
 
     foreach ($profile_list as $item) {
@@ -73,7 +74,7 @@ if (isset($_GET['markerprofileDbId'])) {
             $lineuid = $match[1];
             $expid = $match[2];
         } else {
-            echo "Error: invalid format of marker profile id $lineuid<br>\n";
+            $results['metadata']['status'][] = array("code" => "parm", "message" => "Error: invalid format of marker profile id $item");
             continue;
         }
 
@@ -82,7 +83,16 @@ if (isset($_GET['markerprofileDbId'])) {
               where line_record_uid = $lineuid
               and experiment_uid = $expid
               and not alleles = '--'
-              order by marker_name";
+              order by marker_uid";
+        if ($currentPage == 1) {
+            $sql .= " limit $pageSize";
+        } else {
+            $offset = ($currentPage - 1) * $pageSize;
+            if ($offset < 1) {
+                $offset = 1;
+            }
+            $sql .= " limit $offset, $pageSize";
+        }
         $res = mysqli_query($mysqli, $sql);
         while ($row = mysqli_fetch_row($res)) {
             $count++;
@@ -91,41 +101,60 @@ if (isset($_GET['markerprofileDbId'])) {
         $resultProfile[] = $item;
     }
 } else {
-    $pageSize = 1; //return all markers for each line
     //first query all data
-    $sql = "select count(*) from allele_byline_exp";
-    $res = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
-    if ($row = mysqli_fetch_row($res)) {
-        $num_rows = $row[0];
-    } else {
-        die("Error: experiment not found\n");
-    }
-
-    $sql = "select experiment_uid, line_record_uid, alleles from allele_byline_exp";
+    $num_rows = 0;
     if ($currentPage == 1) {
-        $sql .= " limit $pageSize";
+        $offset = 0;
+        $limit = $pageSize;
     } else {
-        $offset = $currentPage;
-        $sql .= " limit $offset, $pageSize";
+        $offset = ($currentPage - 1) * $pageSize;
+        $limit = $offset + $pageSize;
     }
-    //echo "$sql\n";
-
+    $sql = "select experiment_uid, line_record_uid, count from allele_byline_exp";
     $res = mysqli_query($mysqli, $sql);
     while ($row = mysqli_fetch_row($res)) {
-        $count++;
-        $exp_uid = $row[0];
-        $lin_uid = $row[1];
-        $alleles = $row[2];
-        $item = $exp_uid . "_" . $lin_uid;
-        $resultProfile[] = $item;
-        $sql = "select marker_index from allele_byline_expidx where experiment_uid = $exp_uid";
-        $res2 = mysqli_query($mysqli, $sql);
-        $row2 = mysqli_fetch_row($res2);
-        $marker_ary = explode(",", $row2[0]);
-        $allele_ary = explode(",", $alleles);
-        foreach ($allele_ary as $key => $val) {
-            $dataList[$marker_ary[$key]][] = $val;
+        $expid = $row[0];
+        $lineuid = $row[1];
+        $count = $row[2];
+        $item = $lineuid . "_" . $expid;
+        if (($num_rows == 0) && ($offset == 0)) {
+            $profile_list[] = $item;
+        } elseif (($num_rows > $offset) && ($num_rows < $limit)) {
+            $profile_list[] = $item;
         }
+        $num_rows += $count;
+    }
+    
+    foreach ($profile_list as $item) {
+        if (preg_match("/(\d+)_(\d+)/", $item, $match)) {
+            $lineuid = $match[1];
+            $expid = $match[2];
+        } else {
+            $results['metadata']['status'][] = array("code" => "parm", "message" => "Error: invalid format of marker profile id $item");
+            continue;
+        }
+
+        //now get just those selected
+        $sql = "select marker_uid, alleles from allele_cache
+              where line_record_uid = $lineuid
+              and experiment_uid = $expid
+              and not alleles = '--'
+              order by marker_uid";
+        if ($currentPage == 1) {
+            $sql .= " limit $pageSize";
+        } else {
+            $offset = ($currentPage - 1) * $pageSize;
+            if ($offset < 1) {
+                $offset = 1;
+            }
+            $sql .= " limit $offset, $pageSize";
+        }
+        $res = mysqli_query($mysqli, $sql);
+        while ($row = mysqli_fetch_row($res)) {
+            $count++;
+            $dataList[$row[0]][] = $row[1];
+        }
+        $resultProfile[] = $item;
     }
 }
 
