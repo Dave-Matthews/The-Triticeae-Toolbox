78de49ff680986f71d55208595b0b1dc66c8d258
Clay Birkett <clb343@cornell.edu>
2016-07-06 05:40:25 -0700

    fix phpids error

--- login/edit_whatsnew.php
+++ login/edit_whatsnew.php
@@ -1,12 +1,12 @@
 <?php
 // dem 23oct2014 Edit contents of the "What's New" box.
 require 'config.php';
-include($config['root_dir'] . 'includes/bootstrap_curator.inc');
-connect();
+require $config['root_dir'] . 'includes/bootstrap_curator.inc';
+$mysqli = connecti();
 loginTest();
 
 ob_start();
-include($config['root_dir'] . 'theme/admin_header.php');
+require $config['root_dir'] . 'theme/admin_header.php';
 authenticate_redirect(array(USER_TYPE_CURATOR,USER_TYPE_ADMINISTRATOR));
 ob_end_flush();
 
@@ -14,36 +14,37 @@ echo "<h2>Edit the \"What's New\" box</h2>";
 /*
  * Has an update been submitted?
  */
-if( !empty($_POST) ) {
-  $c = "<!-- empty element for PHP file() to eat -->\n";
-  $c .= $_POST[contents];
-  // Delete, or it will be there in browser page refreshes.
-  $_POST = array();
-  // Now decode it back.
-  $c = html_entity_decode($c);
-  $c = preg_replace('/\\\r/', '', $c );
-  $c = preg_replace('/\\\n/', "\n", $c );
-  $c = preg_replace("/\\\'/", "'", $c );
+if (!empty($_POST)) {
+    $c = "<!-- empty element for PHP file() to eat -->\n";
+    $c .= $_POST[contents];
+    // Delete, or it will be there in browser page refreshes.
+    $_POST = array();
+    // Now decode it back.
+    $c = html_entity_decode($c);
+    $c = preg_replace('/\\\r/', '', $c );
+    $c = preg_replace('/\\\n/', "\n", $c );
+    $c = preg_replace("/\\\'/", "'", $c );
 
   // Open the file and empty it.
   $fname = $config['root_dir'] . 'whatsnew.html';
   $outfile = fopen($fname, "w");
-  if ($outfile === FALSE)
-    die("Unable to open file whatsnew.html.  Probably lack of write-permission.");
+  if ($outfile === FALSE) {
+      die("Unable to open file whatsnew.html.  Probably lack of write-permission.");
+  }
   $status = fwrite($outfile, $c);
-  fclose($outfile);  
+  fclose($outfile);
   echo "<p>Refresh this page to see the result in the WhatsNew box.";
 }
 // Show the current contents in an edit box.
 echo "<form method=POST>";
 echo "<textarea name='contents' rows=30 cols=90";
 $content = file($config['root_dir'] . 'whatsnew.html');
-foreach($content as $linenum=>$str)
-  echo($str);
+foreach ($content as $linenum => $str) {
+    echo($str);
+}
 echo "</textarea><br>";
 echo "<input type='submit' value='Update'>";
 echo "</form>";
 
 echo "</div>";
-include($config['root_dir'] . 'theme/footer.php');
-?>
+require $config['root_dir'] . 'theme/footer.php';
