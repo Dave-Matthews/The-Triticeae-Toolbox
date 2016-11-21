faf198c6c0a52002c8d6b9ee6b6db64ae1e3841b
Clay Birkett <clb343@cornell.edu>
2016-11-21 12:28:22 -0800

    fix bug with include file

--- cron/config.php
+++ cron/config.php
@@ -1,7 +1,7 @@
 <?php
-$root = "//" . $_SERVER['HTTP_HOST'];
-$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-$config['base_url'] = "$root";
-$config['base_url_ssl'] = "$root";
-$config['root_dir'] = dirname(__FILE__).'/';
-
+$root = str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$pos1 = strripos($root, '/', -2);
+$parent_dir = substr($root, 0, $pos1+1);//realpath("$root../");
+$root = '//'.$_SERVER['HTTP_HOST']/*.'/'*/.strtolower(str_replace('//'.$_SERVER['HTTP_HOST'], '', $parent_dir));
+$config['base_url'] = $root;//.'/';
+$config['root_dir'] = realpath(dirname(__FILE__).'/../').'/';
