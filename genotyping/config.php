934e9fe8823f0f31372793b51d454bdfe34447cd
Clay Birkett <clb343@cornell.edu>
2017-09-22 14:08:06 -0700

    marker annotation report - add support for wheat pangenome

--- genotyping/config.php
+++ genotyping/config.php
@@ -8,6 +8,7 @@ $config['root_dir'] = realpath(dirname(__FILE__).'/../').'/';
 $browserLink['IWGSC.36'] = "http://plants.ensembl.org/Triticum_aestivum/Location/View?r=";
 $varLink['IWGSC.36'] = "http://plants.ensembl.org/Triticum_aestivum/Gene/Variation_Gene/Table";
 $browserLink['RefSeq_v1'] = "https://triticeaetoolbox.org/jbrowse/?data=wheat2016&loc=";
+$browserLink['wheat_pan_v1'] = "https://triticeaetoolbox.org/jbrowse/?data=wheat2017&loc=";
 $varLink['RefSeq_v1'] = "https://triticeaetoolbox.org/wheat/genotyping/variations_gene.php";
 $ensemblLinkVEP = "http://plants.ensembl.org/Triticum_aestivum/Tools/VEP?db=core";
 $polymarkerLink = "http://polymarker.tgac.ac.uk";
