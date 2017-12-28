3ed18b3821c8b5298284dbca994cada8394a27de
Clay Birkett <clb343@cornell.edu>
2017-12-28 07:54:13 -0800

    logo - add ARS logo and update links

--- theme/footer.php
+++ theme/footer.php
@@ -28,22 +28,25 @@ if (!isset($_COOKIE["T3terms"])) {
 }
 ?>
 <footer itemscope itemtype="http://schema.org/Organization" style="padding: 15px 0 0 0; height: 160px; top: 0;">
-<table cellpadding="0" cellspacing="0" border="0" width="700" style="width: 700px; background: transparent; border: 0 !important; margin: 0 auto;">
+<table cellpadding="0" cellspacing="0" border="0" style="background: transparent; border: 0 !important; margin: 0 auto;">
 <tr>
 <td align="left" style="border: 0 !important">
 <table cellpadding="0" cellspacing="0" border="0" style="background: transparent; border: 0 !important; text-align: left;">
 <tr>
 <td style="border: 0 !important">
-<a style="border: 0" href="http://www.csrees.usda.gov/" title="USDA National Institute of Food and Agriculture"><img style="border: 0 !important;" src="images/sitelogo.gif" alt="NIFA Logo"></a>
+<a style="border: 0" href="https://nifa.usda.gov/international-wheat-yield-partnership-iwyp-program" title="USDA National Institute of Food and Agriculture"><img style="border: 0 !important;" src="images/sitelogo.gif" alt="NIFA Logo"></a>
+</td><td style="border: 0 !important; text-align: left;">
+<a style="border: 0" href="https://www.ars.usda.gov/research/project/?accnNo=430844" title="USDA Agricultural Research Service"><img style="border: 0 !important;" src="images/ars-logo.png" width="300" alt="ARS Logo"></a>
+</td><td style="border: 0 !important">
+<a style="border: 0" href="http://scabusa.org/" title="US Wheat and Barley Scab Initiative"><img style="border: 0 !important;" src="images/scablogo_notext_small_transparent.gif" alt="Scab Logo"></a>
 </td>
 <td style="border: 0 !important; text-align: left;">
-<p>
 <p style="margin: 0; font-size: 8pt;">To send questions or suggestions to the T3 curators, please <a href="./feedback.php"><b>click here</b></a>.<p> 
 <span style="font-style: italic; font-size: 8pt">
 <a href="http://triticeaetoolbox.org/" title="The Triticeae Toolbox">The Triticeae Toolbox</a> is part of the
 <a itemprop="name" href="http://triticeaecap.org/" title="Triticeae CAP">Triticeae CAP</a>
 project, supported by Agriculture and Food Research Initiative Competitive Grant no. 2011-68002-30029 from the
-<a itemprop="name" href="http://www.csrees.usda.gov/" title="USDA National Institute of Food and Agriculture">USDA National Institute of Food and Agriculture</a>.
+<a itemprop="name" href="https://nifa.usda.gov/international-wheat-yield-partnership-iwyp-program" title="USDA National Institute of Food and Agriculture">USDA National Institute of Food and Agriculture</a>.
 </span>
 </p>
 <p style="margin: 0; font-size: 8pt;"> 
